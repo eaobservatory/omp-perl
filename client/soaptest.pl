ab07cd121859b5029a1f3004fe3e20c177b3a9b8
Tim Jenness <t.jenness@jach.hawaii.edu>
2001-12-20 19:54:44 +0000

    remove unneded xml
    svn path=/trunk/omp/perl/; revision=1498

--- client/soaptest.pl
+++ client/soaptest.pl
@@ -6,161 +6,70 @@ use warnings;
 use strict;
 use SOAP::Lite;
 use OMP::SpServer;
+use OMP::MSBServer;
 
 # Strings
 # Read the data handle
-local $/ = undef;
-my $xml = <DATA>;
+#local $/ = undef;
+#my $xml = <DATA>;
 
-print SOAP::Lite
-  -> uri('http://www.jach.hawaii.edu/OMP::SpServer')
-  -> proxy('http://www-private.jach.hawaii.edu:81/cgi-bin/spsrv.pl')
-  -> storeProgram($xml,"junk")
-  -> result;
+#print SOAP::Lite
+#  -> uri('http://www.jach.hawaii.edu/OMP::SpServer')
+#  -> proxy('http://www-private.jach.hawaii.edu:81/cgi-bin/spsrv.pl')
+#  -> fetchProgram("M01BU53","junk")
+#  -> result;
+
+my $msb = new SOAP::Lite(
+			  uri =>'http://www.jach.hawaii.edu/OMP::MSBServer',
+#			  proxy =>'http://www-private.jach.hawaii.edu:81/cgi-bin/msbsrvtj.pl',
+			  proxy =>'http://www.jach.hawaii.edu/JAClocal/cgi-bin/msbsrv.pl',
+#			  proxy =>'http://www-private.jach.hawaii.edu:81/cgi-bin/msbsrv.pl',
+			 );
+
+my $sp = new SOAP::Lite(
+			  uri =>'http://www.jach.hawaii.edu/OMP::SpServer',
+#			  proxy =>'http://www-private.jach.hawaii.edu:81/cgi-bin/spsrvtj.pl',
+			  proxy =>'http://www.jach.hawaii.edu/JAClocal/cgi-bin/spsrv.pl',
+#			  proxy =>'http://www-private.jach.hawaii.edu:81/cgi-bin/spsrv.pl',
+			 );
+
+$sp = "OMP::SpServer";
+$msb = "OMP::MSBServer";
+
+#print $sp->fetchProgram("M01BTIM2", "junk");
+
+#$sp->outputxml(1);
+
+#my $answer = $msb->queryMSB("<MSBQuery><instrument>UFTI</instrument><wavelength><max>1.0</max></wavelength><projectid>M01BTJ</projectid></MSBQuery>",0);
+#my $answer = $msb->queryMSB("<MSBQuery><projectid>M01BTJ</projectid></MSBQuery>",0);
+
+my $answer = $sp->programDetails("M01BTJ");
+
+#my $answer = $msb->testServer;
+
+# Get the science program
+my $sciprog = new OMP::SciProg( FILE => "test.xml" );
+my $xml = "$sciprog";
+
+#my $answer = $sp->storeProgram($xml, "junk");
+#my $answer = $sp->fetchProgram("M01BTIM2", "junk");
+
+#my $answer = $msb->fetchMSB(8);
+
+if (ref($answer)) {
+  if (ref($answer) eq 'ARRAY') {
+    print $answer->[0];
+  }  elsif ($answer->fault) {
+    print $answer->faultcode . ": " . $answer->faultstring;
+  } else {
+    print $answer->result;
+  }
+} else {
+  print $answer;
+}
 
 #my $result = OMP::SpServer->storeProgram($xml,"junk");
 #print "Result: $result\n";
 #print OMP::SpServer->fetchProgram("M01BU54", "junk");
 
 
-# Some XML
-__DATA__
-<?xml version="1.0" encoding="ISO-8859-1"?>
-<SpProg>
-  <projectID>M01BH01</projectID>
-  <SpTelescopeObsComp id="0">
-    <ItemData name="new" package="gemini.sp.obsComp" subtype="targetList" type="oc"/>
-    <MetaData>
-      <unique>true</unique>
-      <gui>
-        <collapsed>false</collapsed>
-      </gui>
-    </MetaData>
-    <base>
-      <target type="science">
-        <targetName></targetName>
-        <hmsdegSystem type="J2000">
-          <c1>0:00:00</c1>
-          <c2>0:00:00</c2>
-        </hmsdegSystem>
-      </target>
-    </base>
-  </SpTelescopeObsComp>
-  <SpMSB remaining="1">
-    <SpTelescopeObsCompRef idref="0"/>
-    <ItemData name="new" package="gemini.sp" subtype="msb" type="og"/>
-    <MetaData>
-      <gui>
-        <collapsed>false</collapsed>
-      </gui>
-    </MetaData>
-    <SpSiteQualityObsComp>
-      <ItemData name="new" package="gemini.sp.obsComp" subtype="schedInfo" type="oc"/>
-      <seeing>1</seeing>
-      <MetaData>
-        <unique>true</unique>
-        <gui>
-          <collapsed>false</collapsed>
-        </gui>
-      </MetaData>
-      <tauBand>1</tauBand>
-    </SpSiteQualityObsComp>
-    <SpTelescopeObsComp>
-      <ItemData name="new" package="gemini.sp.obsComp" subtype="targetList" type="oc"/>
-      <MetaData>
-        <unique>true</unique>
-        <gui>
-          <collapsed>false</collapsed>
-          <selectedTelescopePos>Base</selectedTelescopePos>
-        </gui>
-      </MetaData>
-      <base>
-        <target type="science">
-          <targetName>NGC Test</targetName>
-          <hmsdegSystem type="J2000">
-            <c1>00:00:00</c1>
-            <c2>+00:00:00</c2>
-          </hmsdegSystem>
-        </target>
-      </base>
-    </SpTelescopeObsComp>
-    <SpObs done="0" remaining="1" msb="false">
-      <ItemData name="new" package="gemini.sp" subtype="none" type="ob"/>
-      <MetaData>
-        <gui>
-          <collapsed>false</collapsed>
-        </gui>
-      </MetaData>
-      <standard>false</standard>
-      <SpIterFolder>
-        <ItemData name="new" package="gemini.sp.iter" subtype="none" type="if"/>
-        <MetaData>
-          <gui>
-            <collapsed>false</collapsed>
-          </gui>
-        </MetaData>
-        <SpIterObserve>
-          <ItemData name="new" package="gemini.sp.iter" subtype="observe" type="ic"/>
-          <repeatCount>1</repeatCount>
-          <MetaData>
-            <gui>
-              <collapsed>false</collapsed>
-            </gui>
-          </MetaData>
-        </SpIterObserve>
-      </SpIterFolder>
-    </SpObs>
-  </SpMSB>
-    <SpObs remaining="3" msb="true">
-      <ItemData name="new" package="gemini.sp" subtype="none" type="ob"/>
-      <MetaData>
-        <gui>
-          <collapsed>false</collapsed>
-        </gui>
-      </MetaData>
-      <standard>false</standard>
-      <SpIterFolder>
-        <ItemData name="new" package="gemini.sp.iter" subtype="none" type="if"/>
-        <MetaData>
-          <gui>
-            <collapsed>false</collapsed>
-          </gui>
-        </MetaData>
-        <SpIterObserve>
-          <ItemData name="new" package="gemini.sp.iter" subtype="observe" type="ic"/>
-          <repeatCount>1</repeatCount>
-          <MetaData>
-            <gui>
-              <collapsed>false</collapsed>
-            </gui>
-          </MetaData>
-        </SpIterObserve>
-      </SpIterFolder>
-    </SpObs>
-    <SpObs remaining="4" msb="true">
-      <ItemData name="new" package="gemini.sp" subtype="none" type="ob"/>
-      <MetaData>
-        <gui>
-          <collapsed>false</collapsed>
-        </gui>
-      </MetaData>
-      <standard>false</standard>
-      <SpIterFolder>
-        <ItemData name="new" package="gemini.sp.iter" subtype="none" type="if"/>
-        <MetaData>
-          <gui>
-            <collapsed>false</collapsed>
-          </gui>
-        </MetaData>
-        <SpIterObserve>
-          <ItemData name="new" package="gemini.sp.iter" subtype="observe" type="ic"/>
-          <repeatCount>2</repeatCount>
-          <MetaData>
-            <gui>
-              <collapsed>false</collapsed>
-            </gui>
-          </MetaData>
-        </SpIterObserve>
-      </SpIterFolder>
-    </SpObs>
-</SpProg>
