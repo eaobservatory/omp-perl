98ba70c48fef1a53b158633b7fe1ed742ab8e61e
Tim Jenness <t.jenness@jach.hawaii.edu>
2002-03-15 19:29:01 +0000

    - Add queryFaults
    svn path=/trunk/omp/perl/; revision=2114

--- OMP/FaultServer.pm
+++ OMP/FaultServer.pm
@@ -203,6 +203,71 @@ sub getFault {
   return $fault;
 }
 
+=item B<queryFaults>
+
+Query the fault database using an XML representation of the query.
+
+  $faults = OMP::FaultServer->queryFaults($faultid,
+                                        $format );
+
+See C<OMP::FaultQuery> for more details on the format of the XML query.
+A typical query could be:
+
+  <FaultQuery>
+    <entity>UFTI</entity>
+    <author>AJA</author>
+  </FaultQuery>
+
+This would return all faults involving UFTI that were filed or responded
+to by AJA.
+
+The format of the returned data is controlled by the last argument.
+This can either be "object" (a reference to an array containing
+C<OMP::Fault> objects), "hash" (reference to an array of hashes), or
+"xml" (return data as XML document). The format of the XML is the same
+as for C<projectDetails> except that a wrapper element of
+C<E<lt>OMPFaultsE<gt>> surrounds the core data. I<Currently only
+"object" is implemented>.
+
+
+=cut
+
+sub queryFaults {
+  my $class = shift;
+  my $xmlquery = shift;
+  my $mode = lc( shift );
+  $mode = "object" unless $mode;
+
+  my @faults;
+  my $E;
+  try {
+
+    my $query = new OMP::FaultQuery( XML => $xmlquery );
+
+    my $db = new OMP::FaultDB(
+			     DB => $class->dbConnection,
+			    );
+
+    @faults = $db->queryFaults( $query );
+
+  } catch OMP::Error with {
+    # Just catch OMP::Error exceptions
+    # Server infrastructure should catch everything else
+    $E = shift;
+
+  } otherwise {
+    # This is "normal" errors. At the moment treat them like any other
+    $E = shift;
+
+  };
+
+  # This has to be outside the catch block else we get
+  # a problem where we cant use die (it becomes throw)
+  $class->throwException( $E ) if defined $E;
+
+  return \@faults;
+}
+
 
 =back
 
