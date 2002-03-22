681c752d7735f4faf2496bed2859e7f966205ed2
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-03-22 02:38:55 +0000

    first version
    svn path=/trunk/omp/perl/; revision=2160

---
+++ cgi/fbcomment.pl
@@ -0,0 +1,18 @@
+#!/local/perl-5.6/bin/perl
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+use lib qw(/jac_sw/omp/test/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $ompcgi = new OMP::CGI( CGI => $q );
+
+$ompcgi->write_page( \&add_comment_content, \&add_comment_output );
---
+++ cgi/msbhist.pl
@@ -0,0 +1,18 @@
+#!/local/perl-5.6/bin/perl
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+use lib qw(/jac_sw/omp/test/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+$cgi->write_page( \&msb_hist_content, \&msb_hist_output );
