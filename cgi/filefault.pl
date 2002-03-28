c5563025d8461d2216c5db03269c47ea0d31d016
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-03-28 19:43:15 +0000

    first version.
    svn path=/trunk/omp/perl/; revision=2209

---
+++ cgi/filefault.pl
@@ -0,0 +1,20 @@
+#!/local/perl-5.6/bin/perl
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
+
+use lib qw(/jac_sw/omp/test/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIFault;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+$cgi->write_page_noauth( \&file_fault, sub { });
