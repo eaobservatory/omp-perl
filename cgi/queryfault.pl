bbef81f0e640e7eacb69148cb2afeed91374fa2e
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-03-29 01:27:53 +0000

    first version
    svn path=/trunk/omp/perl/; revision=2225

---
+++ cgi/queryfault.pl
@@ -0,0 +1,21 @@
+#!/local/perl-5.6/bin/perl
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIFault;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+$cgi->html_title("OMP Fault System: Query");
+$cgi->write_page_noauth( \&query_fault, \&query_fault_output);
