e1bb2773d89e9be5102267cba5738839799cdcca
Tim Jenness <t.jenness@jach.hawaii.edu>
2002-04-11 02:35:48 +0000

    Tweak error message
    svn path=/trunk/omp/perl/; revision=2405

--- OMP/Info/Base.pm
+++ OMP/Info/Base.pm
@@ -220,7 +220,7 @@ sub CreateAccessors {
   my $CLASS_CHECK = q{
 		      #line 1 class_check
 		      unless (UNIVERSAL::isa($argument, 'CLASS')) {
-			croak "Argument for METHOD must be of class CLASS and not class '". (ref($argument) ? ref($argument) : $argument) ."'";
+			croak "Argument for 'METHOD' must be of class CLASS and not class '". (ref($argument) ? ref($argument) : $argument) ."'";
 		      }
 		     };
 
