d8b508f279206d9f1c0bf0b81c13461fa0b4fead
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-06-21 01:15:31 +0000

    now uses OMP::CGIHelper code refs to generate the page and process the output.  the guts of this one have been turned into the OMP::CGIHelper function &issuepwd.
    svn path=/trunk/omp/perl/; revision=2877

--- cgi/issuepwd.pl
+++ cgi/issuepwd.pl
@@ -1,67 +1,19 @@
-#!/local/perl-5.6/bin/perl -XT
-
-# Simple CGI script to allow people to request password
-# updates for their OMP project
-
-use 5.006;
-use warnings;
-use strict;
-
-use CGI;
-use CGI::Carp qw/ fatalsToBrowser /;
-use HTML::WWWTheme;
+#!/local/perl-5.6/bin/perl
 
 BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
 
 use lib "/jac_sw/omp/msbserver";
-use OMP::ProjServer;
-use Error qw/ :try /;
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
 
-
-
-$|=1; # Make unbuffered
-
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
 
 my $q = new CGI;
-print $q->header;
+my $cgi = new OMP::CGI( CGI => $q );
 
-# Use the OMP theme and make some changes to it.
-my $theme = new HTML::WWWTheme("/WWW/JACpublic/JAC/software/omp/LookAndFeelConfig");
-
-$theme->SetHTMLStartString("<html><head><title>OMP Password request page</title></head>");
-$theme->SetSideBarTop("<a href='http://jach.hawaii.edu/'>Joint Astronomy Centre</a>");
-
-print $theme->StartHTML(),
-      $theme->MakeHeader(),
-      $theme->MakeTopBottomBar();
-
-print "<H1>OMP Password Request Page</h1>";
-print "You can use this page to request an updated password for your project.";
-print "The password will be mailed to your registered email address.";
-
-
-print $q->startform;
-print "Project ID: ",$q->textfield('projectid','',8,20);
-print "<P>", $q->submit( '  Request password  ');
-print $q->endform;
-
-if ($q->param) {
-  my $projectid = $q->param("projectid");
-  try {
-    OMP::ProjServer->issuePassword( $projectid );
-    print "<P>Password has been mailed to your registered address</p>\n";
-  } catch OMP::Error::UnknownProject with {
-    print "<P>Unable to process your request because this project ID does not exist in our database<p>\n";
-  } otherwise {
-    my $E = shift;
-    print "<p>Unfortunately an error occurred whilst processing your request<p>\n";
-    print "$E\n";
-  }
-
-}
-
-
-# End the document
-print $theme->MakeTopBottomBar(),
-      $theme->MakeFooter(),
-      $theme->EndHTML();
+my $title = $cgi->html_title;
+$cgi->html_title("$title: Password request page");
+$cgi->write_page_noauth( \&issuepwd, \&issuepwd );
