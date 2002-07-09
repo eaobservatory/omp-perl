0ad076529563865a7287dffec1138a5a23c38ca1
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-07-09 00:24:22 +0000

    first version
    svn path=/trunk/omp/perl/; revision=3127

---
+++ cgi/projecthome.pl
@@ -0,0 +1,22 @@
+#!/local/perl-5.6/bin/perl -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+my $title = $cgi->html_title;
+$cgi->html_title("$title: Project home");
+$cgi->write_page( \&project_home, \&project_home );
