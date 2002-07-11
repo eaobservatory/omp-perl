0635fbeec43152bc09a3715ce71d38235c07f45e
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-07-11 21:19:37 +0000

    this used to be undumpdb.pl
    svn path=/trunk/omp/perl/; revision=3174

---
+++ admin/undumpsciprog.pl
@@ -0,0 +1,52 @@
+#!/local/perl-5.6/bin/perl
+
+# UnDump the contents of the Database
+# [currently only the science programs are restored]
+# from disk
+
+# Note: Only looks for science programs
+# Assumes projects are okay
+# Note: Does not undump project information yet
+
+# The input directory is obtained from $OMP_DUMP_DIR
+
+use 5.006;
+use strict;
+use warnings;
+use OMP::SpServer;
+use OMP::ProjServer;
+use OMP::Error qw/ :try /;
+use Data::Dumper;
+
+# Abort if $OMP_DUMP_DIR is not set
+die "Must specify input data directory via \$OMP_DUMP_DIR"
+  unless exists $ENV{OMP_DUMP_DIR};
+
+chdir $ENV{OMP_DUMP_DIR}
+  or die "Error changing to directory $ENV{OMP_DUMP_DIR}: $!\n";
+
+
+# Read the directory
+opendir my $dh, "."
+  or die "Could not read directory: $!\n";
+my @files = readdir($dh);
+closedir $dh
+  or die "Could not stop reading directory: $!\n";
+
+# slurp mode
+$/ = undef;
+
+# Go through the files looking for xml
+for (@files) {
+  next unless /\.xml$/;
+  print "$_\n";
+
+  # Read the file
+  open my $fh, $_ or die "Could not open file $_: $!\n";
+  my $xml = <$fh>;
+  close($fh) or die "Error closing file: $!\n";
+
+  # Force overwrite
+  OMP::SpServer->storeProgram( $xml, "***REMOVED***",1);
+
+}
