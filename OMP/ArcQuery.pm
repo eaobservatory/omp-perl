414bc71ced356737c2eaf73ae6b1be32c09635d8
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-07-12 00:48:11 +0000

    - sorting JCMT archive lookups by UT_DATE
    svn path=/trunk/omp/perl/; revision=3191

--- OMP/ArcQuery.pm
+++ OMP/ArcQuery.pm
@@ -179,7 +179,7 @@ sub sql {
 
     # SCUBA only for now [note that we explicitly
     # select the database and table
-    $sql = "SELECT * FROM $tabsql $where";
+    $sql = "SELECT * FROM $tabsql $where ORDER BY UTDATE";
 
   } elsif ($tel eq 'UKIRT') {
 
