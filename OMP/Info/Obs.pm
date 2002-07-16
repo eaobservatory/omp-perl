13d9e9e95936c02be44bc2e588ccf671af0cc780
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-07-16 23:38:51 +0000

    - added nightlog method
    svn path=/trunk/omp/perl/; revision=3231

--- OMP/Info/Obs.pm
+++ OMP/Info/Obs.pm
@@ -247,11 +247,172 @@ sub stringify {
   return $self->summary("text");
 }
 
+=item B<nightlog>
+
+Returns a hash representation of data contained in an C<Info::Obs>
+object that can be used for summary purposes.
+
+  %nightlog = $obs->nightlog;
+
+An optional parameter may be supplied. This parameter is a string
+whose values are either 'short' or 'long', which will cause the
+method to return either a small amount of information (i.e. for a
+brief summary) or the full information contained in the object. The
+default is 'short'.
+
+'long' information is not currently implemented.
+
+The returned hash is of the form {header_name => value}, with an additional
+{_ORDER => @ORDER_ARRAY} key-value pair to indicate the order in
+which the values should be displayed.
+
+=cut
+
+sub nightlog {
+  my $self = shift;
+  my $display = shift || 'short';
+
+  my %return;
+
+  # Confirm that the $display variable is either 'short' or 'long'
+  if($display !~ /long|short/i) {
+    $display = 'short';
+  }
+  $display = lc($display);
+
+  # We're going to accomplish this through a large if-then-else block,
+  # keyed on the instrument given in the Info::Obs object. If an
+  # instrument isn't given, then we're stuffed, so throw an error.
+  if(!($self->instrument)) {
+    throw OMP::Error::BadArgs("Info::Obs object did not supply an instrument.");
+  }
+
+  my $instrument = $self->instrument;
+
+  if($instrument =~ /scuba/i) {
+    $return{'Run'} = $self->runnr;
+    $return{'UT time'} = $self->startobs->hms;
+    $return{'Obsmode'} = $self->mode;
+    $return{'Project ID'} = $self->projectid;
+    $return{'Object'} = $self->target;
+    $return{'Tau225'} = $self->tau;
+    $return{'Seeing'} = $self->seeing;
+    $return{'Filter'} = $self->waveband->filter;
+    $return{'Bolometers'} = $self->bolometers;
+    $return{'_ORDER'} = ( "Run", "UT time", "Obsmode", "Project ID", "Object",
+                          "Tau225", "Seeing", "Filter", "Bolometers" );
+  } elsif($instrument =~ /ircam/i) {
+    $return{'Observation'} = $self->runnr;
+    $return{'Group'} = $self->group;
+    $return{'Object'} = $self->target;
+    $return{'Obstype'} = $self->type;
+    $return{'UT start'} = $self->startobs->hms;
+    $return{'Exposure time'} = $self->duration;
+    $return{'Number of exposures'} = $self->nexp;
+    $return{'Mode'} = $self->mode;
+    $return{'Speed'} = $self->speed;
+    $return{'Filter'} = $self->waveband->filter;
+    $return{'Airmass'} = $self->airmass;
+    $return{'Columns'} = $self->columns;
+    $return{'Rows'} = $self->rows;
+    $return{'RA'} = $self->coords->ra;
+    $return{'DEC'} = $self->coords->dec;
+    $return{'Equinox'} = $self->coords->type;
+    $return{'DR Recipe'} = $self->drrecipe;
+    $return{'_ORDER'} = ( "Observation", "Group", "Object", "Obstype",
+                          "UT start", "Exposure time", "Number of exposures",
+                          "Mode", "Speed", "Filter", "Airmass", "Columns",
+                          "Rows", "RA", "DEC", "Equinox", "DR Recipe" );
+  } elsif($instrument =~ /cgs4/i) {
+    $return{'Observation'} = $self->runnr;
+    $return{'Group'} = $self->group;
+    $return{'Object'} = $self->target;
+    $return{'Standard'} = $self->standard;
+    $return{'Observation type'} = $self->type;
+    $return{'Slit'} = $self->slitname;
+    $return{'Position Angle'} = $self->slitangle;
+    $return{'RA offset'} = $self->raoff;
+    $return{'Dec offset'} = $self->decoff;
+    $return{'UT time'} = $self->startobs->hms;
+    $return{'Airmass'} = $self->airmass;
+    $return{'Exposure time'} = $self->duration;
+    $return{'Number of exposures'} = $self->nexp;
+    $return{'Grating'} = $self->grating;
+    $return{'Order'} = $self->order;
+    $return{'Wavelength'} = $self->waveband->wavelength;
+    $return{'DR Recipe'} = $self->drrecipe;
+    $return{'_ORDER'} = ( "Observation", "Group", "Object", "Standard",
+                          "Observation type", "Slit", "Position Angle",
+                          "RA offset", "Dec offset", "UT time", "Airmass",
+                          "Exposure time", "Number of exposures", "Grating",
+                          "Order", "Wavelength", "DR Recipe" );
+  } elsif($instrument =~ /michelle/i) {
+    $return{'Observation'} = $self->runnr;
+    $return{'Group'} = $self->group;
+    $return{'Object'} = $self->target;
+    $return{'Observation type'} = $self->type;
+    $return{'UT start'} = $self->startobs->hms;
+    $return{'Exposure time'} = $self->duration;
+    $return{'Number of exposures'} = $self->nexp;
+    $return{'Mode'} = $self->mode;
+    $return{'Speed'} = $self->speed;
+    $return{'Filter'} = $self->waveband->filter;
+    $return{'Airmass'} = $self->airmass;
+    $return{'Columns'} = $self->columns;
+    $return{'Rows'} = $self->rows;
+    $return{'RA'} = $self->coords->ra;
+    $return{'DEC'} = $self->coords->dec;
+    $return{'Equinox'} = $self->coords->type;
+    $return{'DR Recipe'} = $self->drrecipe;
+    $return{'Standard'} = $self->standard;
+    $return{'Slit'} = $self->slitname;
+    $return{'Position Angle'} = $self->slitangle;
+    $return{'RA offset'} = $self->raoff;
+    $return{'Dec offset'} = $self->decoff;
+    $return{'Grating'} = $self->grating;
+    $return{'Order'} = $self->order;
+    $return{'Wavelength'} = $self->waveband->wavelength;
+    $return{'_ORDER'} = ( "Observation", "Group", "Object", "Observation type",
+                          "UT start", "Exposure time", "Number of exposures",
+                          "Mode", "Speed", "Filter", "Airmass", "Columns",
+                          "Rows", "RA", "DEC", "Equinox", "Slit", "Position Angle",
+                          "RA offset", "Dec offset", "Grating", "Order",
+                          "Wavelength", "DR Recipe" );
+  } elsif($instrument =~ /ufti/i) {
+    $return{'Observation'} = $self->runnr;
+    $return{'Group'} = $self->group;
+    $return{'Object'} = $self->target;
+    $return{'Observation type'} -> $self->type;
+    $return{'UT start'} = $self->startobs->hms;
+    $return{'Exposure time'} = $self->duration;
+    $return{'Number of exposures'} = $self->nexp;
+    $return{'Mode'} = $self->mode;
+    $return{'Speed'} = $self->speed;
+    $return{'Filter'} = $self->waveband->filter;
+    $return{'Airmass'} = $self->airmass;
+    $return{'Columns'} = $self->columns;
+    $return{'Rows'} = $self->rows;
+    $return{'RA'} = $self->coords->ra;
+    $return{'DEC'} = $self->coords->dec;
+    $return{'Equinox'} = $self->coords->type;
+    $return{'DR Recipe'} = $self->drrecipe;
+    $return{'_ORDER'} = ( "Observation", "Group", "Object",
+                          "Observation type", "UT start", "Exposure time",
+                          "Number of exposures", "Mode", "Speed", "Filter",
+                          "Airmass", "Columns", "Rows", "RA", "DEC",
+                          "Equinox", "DR Recipe" );
+  }
+
+  return %return;
+
+}
+
 =back
 
 =head1 AUTHORS
 
 Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
+Brad Cavanagh E<lt>b.cavanagh@jach.hawaii.eduE<gt>
 
 =head1 COPYRIGHT
 
