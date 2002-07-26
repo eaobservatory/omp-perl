e3ab8dbe1914169c0cea9c78e97d8d264e69fbc4
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-07-26 02:58:19 +0000

    first version
    svn path=/trunk/omp/perl/; revision=3309

---
+++ cgi/reports.pl
@@ -0,0 +1,24 @@
+#!/local/perl-5.6/blead/bin/perl5.7.3 -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
+
+use lib qw(/jac_sw/omp_dev/msbserver);
+
+use OMP::CGI;
+use OMP::CGIFault;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+# Set our theme
+my $theme = new HTML::WWWTheme("/WWW/omp/LookAndFeelConfig");
+
+$cgi->html_title("OMP: View Problem Reports");
+$cgi->write_page_report("OMP", \&query_fault_content, \&query_fault_output);
---
+++ cgi/viewreport.pl
@@ -0,0 +1,24 @@
+#!/local/perl-5.6/blead/bin/perl5.7.3 -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase"; }
+
+use lib qw(/jac_sw/omp_dev/msbserver);
+
+use OMP::CGI;
+use OMP::CGIFault;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+# Set our theme
+my $theme = new HTML::WWWTheme("/WWW/omp/LookAndFeelConfig");
+
+$cgi->html_title("OMP: View Report: ". $q->url_param("id"));
+$cgi->write_page_report("OMP", \&view_fault_content, \&view_fault_output);
