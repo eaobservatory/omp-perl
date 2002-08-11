e616b4225dc485dbbf0cba2aa1d2335f21fb239a
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-08-11 20:28:16 +0000

    authentication depends on whether or not obscomment is being run at a telescope
    svn path=/trunk/omp/perl/; revision=3557

--- cgi/obscomment.pl
+++ cgi/obscomment.pl
@@ -11,6 +11,7 @@ use OMP::Info::Comment;
 use OMP::ArchiveDB;
 use OMP::ObslogDB;
 use OMP::Constants;
+use Net::Domain qw/ hostfqdn /;
 
 use Data::Dumper;
 
@@ -20,7 +21,26 @@ my @instruments = qw/cgs4 ircam michelle ufti uist scuba/;
 my $cquery = new CGI;
 my $cgi = new OMP::CGI( CGI => $cquery );
 
-$cgi->write_page( \&obscomment_output, \&obscomment_output );
+# Check to see if we're at one of the telescopes or not. Do this
+# by a hostname lookup, then checking if we're on ulili (JCMT)
+# or mauiola (UKIRT).
+my $location;
+my $hostname = hostfqdn;
+if($hostname =~ /ulili/i) {
+  $location = "jcmt";
+} elsif ($hostname =~ /mauiola/i) {
+  $location = "ukirt";
+} else {
+  $location = "nottelescope";
+}
+
+# Write the page, using the proper authentication on whether or
+# not we're at one of the telescopes
+if(($location eq "jcmt") || ($location eq "ukirt")) {
+  $cgi->write_page_noauth( \&obscomment_output, \&obscomment_output );
+} else {
+  $cgi->write_page( \&obscomment_output, \&obscomment_output );
+}
 
 sub obscomment_output {
 
@@ -30,6 +50,10 @@ sub obscomment_output {
   my %params = $query->Vars;
   my $projectid = $cookie{'projectid'};
   my $password = $cookie{'password'};
+  if(length($projectid . "") == 0) {
+    $projectid = 'staff';
+    $password = 'kalapana';
+  }
 
 # Verify the project id with password
   my $projdb = new OMP::ProjDB( ProjectID => $projectid,
