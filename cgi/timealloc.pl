db5b23002d8336d1fdbdc4dce4aa27b37a86f1f4
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2002-08-31 03:14:51 +0000

    Initial untested version.
    svn path=/trunk/omp/perl/; revision=3861

---
+++ cgi/timealloc.pl
@@ -0,0 +1,45 @@
+#!/local/bin/perl
+
+use CGI;
+#use CGI::Carp qw/fatalsToBrowser/;
+
+#use lib qw(/jac_sw/omp/msbserver);
+use lib qw(/home/bradc/development/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGITimeAlloc;
+#use OMP::TimeAcctDB;
+#use OMP::Project::TimeAcct;
+
+use Net::Domain qw/ hostfqdn /;
+
+use strict;
+
+$| = 1; # make output unbuffered
+my $cquery = new CGI;
+my $cgi = new OMP::CGI( CGI => $cquery );
+$cgi->html_title( "OMP Time Allocation Tool" );
+#my $theme = new HTML::WWWTheme("/WWW/omp-private/LookAndFeelConfig");
+#$cgi->theme($theme);
+
+# Check to see if we're at one of the telescopes or not. Do this
+# by a hostname lookup, then checking if we're on ulili (JCMT)
+# or mauiola (UKIRT).
+my $location;
+my $hostname = hostfqdn;
+if($hostname =~ /ulili/i) {
+  $location = "jcmt";
+} elsif ($hostname =~ /mauiola/i) {
+  $location = "ukirt";
+} else {
+  $location = "nottelescope";
+}
+
+# Write the page, using the proper authentication on whether or
+# not we're at one of the telescopes
+#if(($location eq "jcmt") || ($location eq "ukirt")) {
+  $cgi->write_page_noauth( \&timealloc, \&timealloc_output );
+#} else {
+#  $cgi->write_page( \&timealloc, \&timealloc_output );
+#}
+
