753fbfc4968a3a4beae6177dfb653db55720f29b
Kynan Delorey <k.delorey@jach.hawaii.edu>
2002-10-08 02:17:24 +0000

    first version
    svn path=/trunk/omp/perl/; revision=4273

---
+++ cgi/fbobslog.pl
@@ -0,0 +1,25 @@
+#!/local/perl-5.6/bin/perl -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase";
+	$ENV{OMP_CFG_DIR} = "/jac_sw/omp/msbserver/cfg"
+	  unless exists $ENV{OMP_CFG_DIR};
+      }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+my $title = $cgi->html_title;
+$cgi->html_title("$title: Observation Log");
+$cgi->write_page( \&obslog_content, sub { } );
