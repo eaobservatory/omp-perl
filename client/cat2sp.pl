7c46674b8e805b0fc1472342920d81bc5eb7ad3a
Tim Jenness <t.jenness@jach.hawaii.edu>
2002-11-21 22:57:13 +0000

    Should now work properly.
    svn path=/trunk/omp/perl/; revision=4528

--- client/cat2sp.pl
+++ client/cat2sp.pl
@@ -35,7 +35,7 @@ standard error.
 =item B<catalogue>
 
 Name of a catalogue file containing the target information. This
-should be in JCMT catalogue format (see L<CATALOGUE>). A warning
+should be in JCMT catalogue format (see L<"CATALOGUE">). A warning
 will be issued to standard error if the number of telescope components
 in an MSB is not divisible into the number of targets provided.
 
@@ -49,7 +49,7 @@ The following options are supported:
 
 =item B<-version>
 
-Return the version number.
+Report the version number.
 
 =item B<-help>
 
@@ -92,7 +92,8 @@ Galactic coordinates.
 =back
 
 Coordinates are converted to FK5 J2000 prior to inserting them into
-the science program.
+the science program. Coordinates in AZ frame, planets or orbital
+elements are not supported.
 
 =cut
 
@@ -123,7 +124,7 @@ pod2usage(1) if $help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 
 if ($version) {
-  my $id = '$Id$ '; 
+  my $id = '$Id$ ';
   print "cat2sp - catalogue to science program importer\n";
   print " CVS revision: $id\n";
   exit;
@@ -135,6 +136,7 @@ my $NAMEEL     = "targetName";
 my $RAEL       = "c1";
 my $DECEL      = "c2";
 my $BLANK_NAME = "<$NAMEEL></$NAMEEL>";
+my $BLANK_NAME2 = "<$NAMEEL/>";
 my $BLANK_RA   = "<$RAEL>0:00:00</$RAEL>";
 my $BLANK_DEC  = "<$DECEL>0:00:00</$DECEL>";
 
@@ -153,52 +155,136 @@ unless (defined $template && defined $coords) {
 my $sp = new OMP::SciProg( FILE => $template )
   or die "Error opening template file $template";
 
-# Slurp in the file
-local $/ = undef;
-my $refxml = <$fh>;
-close($fh) or die "error closing template file $msb: $!\n";
-local $/ = "\n";
-
-die "No target in supplied XML"
-  unless $refxml =~ /SpTelescopeObsComp/;
-
-die "No blank target entries"
-  unless isBlank($refxml);
-
 # now open the coordinates file. The opening and parsing is trivial
 # we use SrcCatalog::JCMT for this since it also converts to J2000.
 # If we do not want this dependency we will need to do more tweaking
 # of the XML since GA and RB require changes to the telescope XML
 # that are not currently in place.
+my $cat = new SrcCatalog::JCMT( $coords );
+my @sources = @{$cat->current};
 
+# Remove any targets that are not RADEC
+@sources = grep { $_->type eq 'RADEC'  } @sources;
 
-# loop over each line
-my $xml = $refxml;
-my $continue = 0;
-while (defined (my $line = <$fh>)) {
-  # replace comments
-  $line =~ s/\#.*$//;
-  $line =~ s/\s*$//;
-  next unless $line;
+# Now we need to loop over each MSB
+for my $msb ($sp->msb) {
 
-  my ($name, $ra, $dec) = split(/\s+/,$line);
+  # If this MSB has a blank target it should be cloned
+  # whilst iterating through the source list
+  # Quickest way to look for blank is simply to generate
+  # XML string and do a pattern match
+  next unless isBlank( "$msb" );
 
-  # replace the entries (but only once)
-  $xml =~ s/$BLANK_NAME/<$NAMEEL>$name<\/$NAMEEL>/;
-  $xml =~ s/$BLANK_RA/<$RAEL>$ra<\/$RAEL>/;
-  $xml =~ s/$BLANK_DEC/<$DECEL>$dec<\/$DECEL>/;
+  # The number of times we clone this depends on the number of
+  # blank entries in the MSB. Easiest to determine that number
+  # ahead of the cloning loop.
+  my $blanks = countBlanks( $msb->_tree->toString );
+  # warn "BLANKS: $blanks\n";
+  next if $blanks == 0;
 
-  # if the xml still has blank entries we skip
-  # else we dump to stdout and reset the xml string
-  unless (isBlank($xml)) {
-    print $xml;
-    $xml = $refxml;
+  # Warn if we do not have an exact match
+  warn "Number of blank targets in MSB '".$msb->msbtitle."' is not divisible into the number of targets supplied\n Some targets will be missing.\n"
+    if scalar(@sources) % $blanks != 0;
+
+  # Calculate the max number we can support [as an index]
+  my $max = int(($#sources+1) / $blanks) * $blanks - 1;
+
+  # Now loop over the source list
+  for (my $i = 0; ($i+$blanks-1) <= $max; $i+= $blanks) {
+
+    # Okay now we need to deep clone the MSB
+    my $clone = $msb->_tree->cloneNode( 1 );
+
+    # There is no method in OMP::MSB (yet) for replacing target
+    # XML with other target XML. The only way we can do this
+    # currently is to take each MSB, use XPath to locate the
+    # telescope components, then find the nodes corresponding to
+    # the coordinates and change the values
+    # Note that we do not deal with inherited telescope components
+    # Note also that _tree is a private method so we really need
+    # to provide an explicit OMP::MSB interface for this
+    my @tels = $clone->findnodes(".//SpTelescopeObsComp");
+
+    # Now loop over the telescope components
+    my $c = 0; # Count number replaced
+    for my $tel (@tels) {
+
+      die "Internal error: The number of blank telescope components encountered exceeds the expected number!!!\n" if $c >= $blanks;
+
+      #warn $tel->toString ."\n";
+
+      # Locate the targetName, ra and dec components
+      my ($targetnode) = $tel->findnodes(".//$NAMEEL");
+      next unless $targetnode;
+
+      # If this is undefined we have to attach a child ourselves
+      my $target = $targetnode->firstChild;
+
+      # Either this should be an empty string node or no node at all
+      if (defined $target) {
+	my $string = $target->toString;
+	next if length($string) > 0 && $string =~ /\w/;
+      }
+
+      my ($ranode)     = $tel->findnodes(".//$RAEL");
+      next unless $ranode;
+      next unless $ranode->toString eq $BLANK_RA;
+
+      my ($decnode)    = $tel->findnodes(".//$DECEL");
+      next unless $decnode;
+      next unless $decnode->toString eq $BLANK_DEC;
+
+      #warn "Target: " .$targetnode->toString."\n";
+      #warn "RA:     " .$ranode->toString."\n";
+      #warn "Dec:    " .$decnode->toString."\n";
+
+      # Now we know we have a blank target component
+      # Get the children
+      my $ra = $ranode->firstChild;
+      #warn "RA child is $ra\n";
+      my $dec = $decnode->firstChild;
+      #warn "Dec child is $dec\n";
+
+      # Get the target
+      if (($i+$c) > $max) {
+	die "Error - we seem to have miscalculated\n";
+      }
+      my $coords = $sources[ $i + $c];
+
+      # Change the content
+      $ra->setData( $coords->ra( format => 'sex'));
+      $dec->setData( $coords->dec( format => 'sex'));
+
+      if (defined $target) {
+	$target->setData( $coords->name );
+      } else {
+	my $text = new XML::LibXML::Text( $coords->name );
+	$targetnode->appendChild( $text );
+      }
+
+      #warn $tel->toString ."\n";
+
+      # increment the tel counter
+      $c++;
+    }
+
+    die "Internal error: We found fewer blank telescope components than expected!!!\n" unless $c == $blanks;
+
+    # Now we need to attach the clone to the parent
+    $msb->_tree->parentNode->insertAfter( $clone, $msb );
+
   }
+
+  # Now we need to remove the template node
+  $msb->_tree->unbindNode;
+
+  #  info message
+  warn "Cloning MSB with title " . $msb->msbtitle ."\n"
+
 }
 
-# should probably trap for case where we did not dump the
-# last xml because only partial replacement
-
+# and send the science programme to stdout
+print "$sp\n";
 
 exit;
 
@@ -207,7 +293,7 @@ sub isBlank {
   my $string = shift;
 
   my $blank;
-  if ($string =~ /$BLANK_NAME/ && $string =~ /$BLANK_RA/
+  if ($string =~ /($BLANK_NAME|$BLANK_NAME2)/ && $string =~ /$BLANK_RA/
 	  && $string =~ /$BLANK_DEC/) {
     $blank = 1;
   } else {
@@ -216,12 +302,46 @@ sub isBlank {
   return $blank;
 }
 
+# Count the number of blank targets in the MSB
+# Returns the minimum of number of blank names, number of blank
+# RAs and number of blank decs
+sub countBlanks {
+  my $string = shift;
+
+  my $nnames = ( $string =~ s/($BLANK_NAME|$BLANK_NAME2)/$BLANK_NAME2/g );
+  #warn "Got names: $nnames\n";
+  my $nra    = ( $string =~ s/$BLANK_RA/$BLANK_RA/g );
+  #warn "Got RA: $nra\n";
+  my $ndec   = ( $string =~ s/$BLANK_DEC/$BLANK_DEC/g );
+  #warn "Got Dec: $ndec\n";
+
+  my $min = $nnames;
+  $min = $nra if $nra < $min;
+  $min = $ndec if $ndec < $min;
+
+  return $min;
+}
+
+
+=head1 NOTES
+
+This routine does not touch inherited telescope components. Each
+MSB that you wish to process must contain an explicit telescope
+component.
+
+If the number of telescopes in your MSB is not divisible into
+the number of supplied target positions some targets will not be
+included in the output science program (the routine only provides
+output for complete MSBs). This does mean that if you have an
+MSB requiring 2 target positions and a catalogue containing only
+one target then that MSB will not be output at all.
+
 =head1 DEPENDENCIES
 
 Requires the availability of the OMP infrastructure modules
 (specifically L<OMP::SciProg|OMP::SciProg> which depends on the CPAN
-module L<XML::LibXML> which requires a modern version of the
-C<libxml2> library from C<www.xmlsoft.org>) as well as the
+module L<XML::LibXML|XML::LibXML> which requires a modern version of
+the C<libxml2> library from C<www.xmlsoft.org>) as well as the
 C<SrcCatalog::JCMT> module (which depends on the CPAN modules
 L<Astro::SLA|Astro::SLA> and L<Astro::Coords|Astro::Coords>).
 
