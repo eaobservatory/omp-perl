6729e24458b3ad0278e9a1b6e7a18d29469b2b3f
Kynan Delorey <k.delorey@jach.hawaii.edu>
2003-01-16 23:43:01 +0000

    first version
    svn path=/trunk/omp/perl/; revision=4988

---
+++ cgi/nightrep.pl
@@ -0,0 +1,24 @@
+#!/local/perl-5.6/bin/perl -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase";
+	$ENV{OMP_CFG_DIR} = "/jac_sw/omp/msbserver/cfg"
+	  unless exists $ENV{OMP_CFG_DIR};
+      }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+$cgi->html_title("Observing Report " . $q->url_param('utdate'));
+$cgi->write_page_noauth( \&night_report, \&night_report );
