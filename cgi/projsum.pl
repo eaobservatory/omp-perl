e7a77a11d25bfb9ff9f4f49d4dcfa950115579bf
Kynan Delorey <k.delorey@jach.hawaii.edu>
2003-01-17 23:52:05 +0000

    first version
    svn path=/trunk/omp/perl/; revision=4999

---
+++ cgi/projsum.pl
@@ -0,0 +1,25 @@
+#!/local/perl-5.6/bin/perl -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase";
+	$ENV{OMP_CFG_DIR} = "/jac_sw/omp/msbserver/cfg"
+	  unless exists $ENV{OMP_CFG_DIR};
+      }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIHelper;
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $ompcgi = new OMP::CGI( CGI => $q );
+
+my $title = $ompcgi->html_title;
+$ompcgi->html_title("$title: Project Details");
+$ompcgi->write_page_noauth( \&proj_sum_page, \&proj_sum_page );
