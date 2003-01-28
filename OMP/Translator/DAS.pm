afcf08950181b37def1e16c4fbe9a4e0077ec04f
Tim Jenness <t.jenness@jach.hawaii.edu>
2003-01-28 17:03:49 +0000

    Add GRID support and tweak target
    svn path=/trunk/omp/perl/; revision=5117

--- OMP/Translator/DAS.pm
+++ OMP/Translator/DAS.pm
@@ -24,7 +24,6 @@ use 5.006;
 use strict;
 use warnings;
 
-use OMP::SciProg;
 use OMP::Error;
 use Astro::Telescope;
 
@@ -132,22 +131,40 @@ sub translate {
       # Store the target
       push(@targets, $obsinfo->{coords}) if defined $obsinfo->{coords};
 
-      #use Data::Dumper;
-      #print STDERR Dumper($obsinfo);
+      # What are we going to do about PATTERN files???
+      # In general we can not write them to disk in this phase
+      # but we also need to make sure we write them with a unique
+      # name so that the HTML can track them.
+      # The HTML is pretty fixed at this point (unless we insert 
+      # A large string and do a replacement)
+      # We could generate the text for the file and return it
+      # in addition to the HTML, indexed by the name used in the XML??
+      # Targets are different because we need to index them by name once
+      # we are complete.
+
+      use Data::Dumper;
+      print STDERR Dumper($obsinfo);
 
       # Start new observation
-      $html .= "<h2>Observation #$obscount</h2>\n";
+      my $mode = $self->obsMode( %$obsinfo );
+      $html .= "<h2>Observation #$obscount: $mode</h2>\n";
 
       # Target information (assuming we have not already specified it)
       $html .= $self->targetConfig( %$obsinfo );
 
-      # Front end configuration (assuming we have not already specified it)
-      $html .= $self->feConfig( %$obsinfo );
+      # BE/FE configuration are not required for FOCUS and POINTING
+      if ($mode ne 'FOCUS' && $mode ne 'POINTING') {
 
-      # Back end
-      $html .= $self->beConfig( %$obsinfo );
+	# Front end configuration (assuming we have not already specified it)
+	$html .= $self->feConfig( %$obsinfo );
+
+	# Back end
+	$html .= $self->beConfig( %$obsinfo );
+
+      }
 
       # Actual observing mode (inc cell)
+      #my ($snippet, %files) = $self->
 
     }
 
@@ -350,6 +367,47 @@ sub fixedFooter {
   return $html;
 }
 
+=item B<obsMode>
+
+Return the observing mode for this observation (as understood by the OCS).
+
+  $mode = OMP::Translator::DAS->obsMode( %summary );
+
+Allowed responses are: SAMPLE, RASTER, GRID, PATTERN, POINTING, FOCUS
+
+=cut
+
+sub obsMode {
+  my $self = shift;
+  my %summary = @_;
+
+  my $otMode = $summary{MODE};
+
+  my $mode;
+  if ($otMode eq 'SpIterStareObs') {
+    # Work it out from the offsets
+    if (exists $summary{offsets} ) {
+      use Data::Dumper;
+      print Dumper(\%summary);
+      my %data = offsets_to_grid( @{ $summary{offsets} } );
+      $mode = $data{TYPE};
+    } else {
+      $mode = "SAMPLE";
+    }
+
+  } elsif ($otMode eq 'SpIterRasterObs' ) {
+    $mode = "RASTER";
+  } elsif ($otMode eq 'SpIterFocusObs') {
+    $mode = "FOCUS";
+  } elsif ($otMode eq 'SpIterPointingObs') {
+    $mode = "POINTING";
+  } else {
+    throw OMP::Error::TranslateFail("Unable to translate mode $otMode to heterodyne observation");
+  }
+
+  return $mode;
+}
+
 =item B<feConfig>
 
 Create HTML for the Frontend configuration.
@@ -446,12 +504,30 @@ sub targetConfig {
   my $self = shift;
   my %summary = @_;
 
-  my $c = $summary{coords};
-  $c->telescope( new Astro::Telescope('JCMT') );
-
   my $html;
-  $html .= "<H3>Target Information: <em>".$summary{target}."</em></H3>\n";
-  $html.= "<PRE>". $c->status ."</PRE>\n";
+  if ($summary{autoTarget})  {
+    $html = "<H3>Target information: Please choose a suitable target</H3>\n";
+
+    $html .= "No target has been chosen for this observation. Choice of a suitable target is at your discretion.<br>\n";
+    if ($summary{standard}) {
+      $html .= "<em>Please note that this observation is inside a calibration block so it might be advisable to choice a calibration source.</em>\n";
+
+    }
+
+  } elsif ($summary{standard}) {
+    $html = "<H3>Target information: Please choose a suitable target</H3>\n";
+
+    $html .= "No target has been chosen for this calibration observation. Choice of a suitable calibration target is at your discretion.\n";
+
+  } else {
+
+    my $c = $summary{coords};
+    $c->telescope( new Astro::Telescope('JCMT') );
+
+    $html .= "<H3>Target Information: <em>".$summary{target}."</em></H3>\n";
+    $html.= "<PRE>". $c->status ."</PRE>\n";
+  }
+
   return $html;
 
 }
@@ -473,27 +549,228 @@ sub _HztoMHz {
   return $hz;
 }
 
-# Given an array of offsets (dx,dy,PA) determine
+# Given an array of offsets (OFFSET_DX,OFFSET_DY,PA) determine
 # whether they are on a regular grid or random
 # If random, return an array offsets that have a fixed PA
 # (usually 0.0)
+# Note that we can return a PATTERN even if the offsets are gridded.
+# This is because the order is important. Filling a grid in a non-standard
+# order assumes we do not want to use a GRID
+
+# Note that from the translator point of view this is all crazy. we should
+# always return a pattern file since the TSS will not notice the difference.
+# It would save a lot of worrying in this routine!
 
 # Returns a hash with keys:
-#  TYPE =>  PATTERN or GRID
-#  GRIDDX => X spacing in arcseconds   [GRID]
-#  GRIDDY => Y spacing in arcseconds   [GRID]
-#  GRIDNX => Number of positions in X  [GRID]
-#  GRIDPA => Position angle of grid    [GRID]
-#  OFFSETX => X Centre of grid pattern [GRID]
-#  OFFSETY => Y centre of grid pattern [GRID]
-#  OFFSETS => Array of hashes with OFFSET_DX and OFFSET_DY [PATTERN]
+#  TYPE =>  SAMPLE or PATTERN or GRID
+#  GRID_DX => X spacing in arcseconds   [GRID]
+#  GRID_DY => Y spacing in arcseconds   [GRID]
+#  GRID_NX => Number of positions in X  [GRID]
+#  GRID_NX => Number of positions in Y  [GRID]
+#  GRID_X => X Centre of grid pattern [GRID]
+#  GRID_Y => Y centre of grid pattern [GRID]
+#  PA => Position angle of offsets    [GRID and PATTERN and SAMPLE]
+#  OFFSETS => Array of arrays with OFFSET_DX and OFFSET_DY [PATTERN/SAMPLE]
 
 # Note that if the offsetx/y of the GRID is not the centre of the
 # grid (ie the grid has even NX or NY) then we return the coordinates
 # as a PATTERN because the Heterodyne system can not support an even GRID
 
 sub offsets_to_grid {
+  my @offsets = @_;
 
+  # If there is only one offset we have a SAMPLE not PATTERN or GRID
+  if (scalar(@offsets) == 1) {
+    return (TYPE => 'SAMPLE', 
+	    PA => $offsets[0]->{OFFSET_PA},
+	    OFFSETS => [ $offsets[0]->{OFFSET_DX},
+			 $offsets[0]->{OFFSET_DY} ]);
+  } elsif (scalar(@offsets) == 0) {
+    # No offsets - just use the origin
+    return (TYPE => 'SAMPLE', PA=>0.0, OFFSETS=>[0.0,0.0]);
+  }
+
+  # All the offsets should be at a single PA (that is what the OT
+  # generates) but it does no harm to check this)
+  my %pa;
+  for my $off (@offsets) {
+    $pa{$off->{OFFSET_PA}}++;
+  }
+  throw OMP::Error::TranslateFail("This offset iterator contained offsets at differing position angles - not supported") 
+    if (scalar(keys %pa) > 1);
+
+  # For the results
+  my %results;
+  $results{PA} = $offsets[0]->{OFFSET_PA};
+
+  # First need to determine whether we are a gridded observation
+  # Use hashes at first to remove duplicates
+  # And calculate the min and max extent
+  my %x;
+  my %y;
+  for my $off (@offsets) {
+   $x{$off->{OFFSET_DX}}++;
+   $y{$off->{OFFSET_DY}}++;
+  }
+
+  # Sort
+  my @xsort = sort { $a <=> $b } keys %x;
+  my @ysort = sort { $a <=> $b } keys %y;
+
+  # If we have an even number of X or Y offsets we cannot implement
+  # this as a heterodyne GRID - must be a pattern because the grid
+  # centre must be inclusive
+  if ((scalar(@xsort) % 2) != 1 || (scalar(@ysort) % 2) != 1 ) {
+    $results{TYPE} = "PATTERN";
+  }
+
+  # Find extent
+  my $xmin = $xsort[0];
+  my $xmax = $xsort[-1];
+  my $ymin = $ysort[0];
+  my $ymax = $ysort[-1];
+
+  # This is probably not the most efficient algorithm...
+
+  # Go through all the X+Y coordinates making sure that they increment
+  # by a constant amount
+  #use Data::Dumper;
+  #print Dumper(\@xsort,\@ysort);
+  my $contig = 0;
+
+  if (!exists $results{TYPE}) {
+    $contig = 1; # Assume gridded until proven otherwise
+  XY: for my $xy (\@xsort, \@ysort) {
+      my $delta;
+      # if we have more than one coordinate in the dimension
+      if (scalar(@$xy) > 1) {
+	for my $i (1..$#$xy) {
+	  my $diff = $xy->[$i] - $xy->[$i-1];
+	  if (defined $delta) {
+	    # Must compare with previous value
+	    if ($diff != $delta) {
+	      # non-contiguous
+	      $contig=0;
+	      print "Delta: $delta  Diff: $diff I: $i\n";
+	      last XY;
+	    }
+	  } else {
+	    # First time through can have no error
+	    $delta = $diff;
+	  }
+	}
+      }
+    }
+  }
+
+  # if the numbers were contiguous we can do more investigating
+  # into the GRID-ness
+  if ($contig) {
+    # We know the X and Y deltas are even so use them to generate
+    # a best guess grid
+    # First make a guess at the dx and dy for the grid
+    # We are simply trying to disprove the grid theory and call it a pattern
+    my $dx = 0;
+    my $dy = 0;
+
+    $dx = $xsort[1] - $xsort[0] if scalar(@xsort) > 1;
+    $dy = $ysort[1] - $ysort[0] if scalar(@ysort) > 1;
+
+    # Make a two dimensional array and fill it with zeroes
+    # We then go through all the offsets and make sure they correspond
+    # with the expected position in a GRID pattern
+
+    # Number in each dimension
+    my $nx = scalar(@xsort);
+    my $ny = scalar(@ysort);
+
+    print "DX= $dx NX=$nx DY = $dy  NY=$ny\n";
+
+    # Simply calculate the expected offsets and compare with what
+    # we got
+    my $n = 0; # offset counter
+    # Y changes slower than X
+    # Assumes you start at xmin,ymin and increment by dx until you need
+    # to increment dy (resetting to the start of the row). This means
+    # no scan reversal.
+    ROW: for my $iy (1..$ny) {
+	# Expected Y offset
+	my $ey = $ymin + ($dy*($iy-1));
+	print "Y offset = $ey\n";
+	for my $ix (1.. $nx) {
+	  # This is the requested offset
+	  my $off = $offsets[$n];
+
+	  # This is the X expected offset
+	  my $ex = $xmin + ($dx*($ix-1));
+	  print "X offset = $ex [counter= $n]\n";
+
+	  print "Required offset: ". $off->{OFFSET_DX} .",".
+	    $off->{OFFSET_DY}."\n";
+	  # Compare offsets
+	  if ($off->{OFFSET_DX} != $ex ||
+	      $off->{OFFSET_DY} != $ey) {
+	    # This is not a grid in the expected order
+	    $contig = 0;
+	    print "NON-CONTIGUOUS: GRID\n";
+	    last ROW;
+	  }
+	
+	  # increment the offset index
+	  $n++;
+	}
+      }
+
+    if ($contig) {
+      $results{TYPE} = "GRID";
+
+      # Store the parameters
+      $results{GRID_DX} = $dx;
+      $results{GRID_DY} = $dy;
+      $results{GRID_NX} = $nx;
+      $results{GRID_NY} = $ny;
+
+      # The reference offset is from the middle of the grid
+      # we know that NX and NY are odd
+      throw OMP::Error::FatalError("NX and NY must be odd by this point in the code - internal error [got $nx and $ny]")
+	if (($nx % 2) != 1 || ($ny % 2) != 1);
+      $results{GRID_X} = $xmin + (($nx-1)/2)*$dx;
+      $results{GRID_Y} = $ymin + (($ny-1)/2)*$dy;
+
+      print Dumper(\%results);
+
+      return %results;
+
+    } else {
+      $results{TYPE} = "PATTERN";
+    }
+  } else {
+    $results{TYPE} = "PATTERN";
+  }
+
+  # Now we know where we are going
+  if (!exists $results{TYPE}) {
+    throw OMP::Error::FatalError("Somehow we did not manage to select a type: GRID, PATTERN or SAMPLE");
+
+  } elsif ($results{TYPE} eq 'PATTERN') {
+    # Create array of array offsets
+    my @off;
+    for my $off (@offsets) {
+      push(@off, [ $off->{OFFSET_DX}, $off->{OFFSET_DY}] );
+    }
+
+    $results{OFFSETS} = \@off;
+    print Dumper(\%results);
+
+  } elsif ($results{TYPE} eq 'GRID') {
+    throw OMP::Error::FatalError("Unexpectedly obtained GRID solution");
+
+  } else {
+    throw OMP::Error::TranslateFail("Internal error processing offsets. Was neither a GRID nor a PATTERN file");
+
+  }
+
+  return %results;
 
 }
 
