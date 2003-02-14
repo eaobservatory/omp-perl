f8a390c4b8355e40a3145b0888b9e9866ce91354
Kynan Delorey <k.delorey@jach.hawaii.edu>
2003-02-14 02:24:12 +0000

    backed out of last change where I sent another message to whoever was on the cclist
    svn path=/trunk/omp/perl/; revision=5381

--- OMP/FeedbackDB.pm
+++ OMP/FeedbackDB.pm
@@ -339,11 +339,11 @@ sub _mail_comment {
 
   $self->_mail_information(%details);
 
-  if ($cclist) {
-    # Mail another copy since CC'ing doesn't work at all for some reason
-    $details{to} = join(',',@$cclist);
-    $self->_mail_information(%details);
-  }
+#  if ($cclist) {
+#    # Mail another copy since CC'ing doesn't work at all for some reason
+#    $details{to} = join(',',@$cclist);
+#    $self->_mail_information(%details);
+#  }
 
 }
 
