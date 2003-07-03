445e9f7e54ba90974b01217879d8f6ca290fe3ca
Kynan Delorey <k.delorey@jach.hawaii.edu>
2003-07-03 20:49:47 +0000

    added userhtml function.  cuts down on coding when one needs to display a person's name as a link to their user details, but it has a stupid clunky interface.
    svn path=/trunk/omp/perl/; revision=6160

--- OMP/Display.pm
+++ OMP/Display.pm
@@ -21,6 +21,8 @@ use warnings;
 use strict;
 use Carp;
 
+use OMP::Config;
+
 require HTML::TreeBuilder;
 require HTML::FormatText;
 
@@ -58,6 +60,62 @@ sub html2plain {
   my $plaintext = $formatter->format($tree);
 
   return $plaintext;
+}
+
+=item B<userhtml>
+
+Returns html for a hyperlink to the user details page for a user.  Also
+displays an icon next to the hyperlink indicating whether or not the user
+receieves project emails.
+
+  $html = OMP::Display->userhtml($user, $cgi, $getemails, $projectid);
+
+First argument should be an C<OMP::User> object.  Second argument
+should be an C<OMP::CGI> object.  If the optional third
+argument is non-zero a "receives email" icon is displayed; if 0 an
+"ignores email" icon is displayed.  If undefined no icon is displayed.  Finally,
+If the third argument is defined the fourth argument, a project ID, is required.
+Thank you and goodnight!
+
+=cut
+
+sub userhtml {
+  my $self = shift;
+  my $user = shift;
+  my $cgi = shift;
+  my $emailstatus = shift;
+  my $project = shift;
+  if (defined $emailstatus and ! defined $project) {
+    $emailstatus = undef;
+  }
+
+  my $private = OMP::Config->getData('omp-private');
+  my $public = OMP::Config->getData('omp-url');
+  my $cgidir = OMP::Config->getData('cgidir');
+  my $iconsdir = OMP::Config->getData('iconsdir');
+
+  # Link to private pages if we are currently on a private page
+  my $url;
+  if ($cgi->url(-base=>1) =~ /^$private/) {
+    $url = $private . $cgidir . "/userdetails.pl?user=" . $user->userid;
+  } else {
+    $url = $public . $cgidir . "/userdetails.pl?user=" . $user->userid;
+  }
+
+  my $html = "<a href=$url>$user</a>";
+
+  # Append email status icon
+  if (defined $emailstatus) {
+    if ($emailstatus) {
+      $html .= " <a href=$public$cgidir/projusers.pl?urlprojid=$project>".
+	"<img src=$public$iconsdir/mail.gif alt='receives email' border=0></a>"
+    } else {
+      $html .= " <a href=$public$cgidir/projusers.pl?urlprojid=$project>".
+	"<img src=$public$iconsdir/nomail.gif alt='ignores email' border=0></a>";
+    }
+  }
+
+  return $html;
 }
 
 =back
