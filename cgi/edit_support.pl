13a9f141974a6382acdda16c6e0e2f0b93bfd79d
Kynan Delorey <k.delorey@jach.hawaii.edu>
2003-09-03 21:39:07 +0000

    first version
    svn path=/trunk/omp/perl/; revision=6476

---
+++ cgi/edit_support.pl
@@ -0,0 +1,29 @@
+#!/local/perl-5.6/bin/perl -XT
+
+use CGI;
+use CGI::Carp qw/fatalsToBrowser/;
+
+BEGIN { $ENV{SYBASE} = "/local/progs/sybase";
+	$ENV{OMP_CFG_DIR} = "/jac_sw/omp/msbserver/cfg"
+	  unless exists $ENV{OMP_CFG_DIR};
+      }
+
+use lib qw(/jac_sw/omp/msbserver);
+
+use OMP::CGI;
+use OMP::CGIUser;
+use OMP::UserServer;
+use OMP::Error qw(:try);
+
+use strict;
+use warnings;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $cgi = new OMP::CGI( CGI => $q );
+
+my $title = $cgi->html_title;
+
+$cgi->html_title("$title: Edit support contacts");
+$cgi->write_page_noauth( \&OMP::CGIHelper::support, \&OMP::CGIHelper::support );
