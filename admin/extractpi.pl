7dcf495acda925c0df62a220c71c30e077658d2e
Tim Jenness <t.jenness@jach.hawaii.edu>
2003-12-24 23:21:23 +0000

    first version
    svn path=/trunk/omp/perl/; revision=6787

---
+++ admin/extractpi.pl
@@ -0,0 +1,25 @@
+#!/local/perl-5.6/bin/perl
+
+# Query the database for all projects associated with a specific
+# semester and telescope and dump to stdout in a form suitable for use
+# as a majordomo mailing list import.
+
+use warnings;
+
+use OMP::ProjServer;
+
+my $SEM = '04A';
+my $TEL = 'JCMT';
+
+# Query the database
+$projects = OMP::ProjServer->listProjects( "<ProjQuery><semester>$SEM</semester><telescope>$TEL</telescope></ProjQuery>", "object");
+
+
+# Extract PI users and remove duplicates
+my %users = map { $_->pi->userid, $_->pi } @$projects;
+
+# Dump user information sorted
+for my $id ( sort keys %users ) {
+  print $users{$id}->as_email_hdr ."\n";
+
+}
