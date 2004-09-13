6a2d04f2218f44bc6a3761ddef594f49c7be9ad2
Kynan Delorey <k.delorey@jach.hawaii.edu>
2004-09-13 22:17:57 +0000

    first version
    svn path=/trunk/omp/perl/; revision=7632

---
+++ OMP/FBQuery.pm
@@ -0,0 +1,115 @@
+package OMP::FBQuery;
+
+=head1 NAME
+
+OMP::FBQuery - Class representing queries of the feedback message system
+
+=head1 SYNOPSIS
+
+$query = new OMP::FBQuery( XML => $xml );
+  $sql = $query->sql( $table );
+
+=head1 DESCRIPTION
+
+This class can be used to process OMP feedback message queries. The queries
+are usually represented as XML.
+
+=cut
+
+use 5.006;
+use strict;
+use warnings;
+use Carp;
+
+# External modules
+use OMP::Error;
+use OMP::General;
+
+# Inheritance
+use base qw/ OMP::DBQuery /;
+
+# Package globals
+
+our $VERSION = (qw$Revision$ )[1];
+
+=head1 METHODS
+
+=head2 General Methods
+
+=over 4
+
+=item B<sql>
+
+Returns an SQL representation of the XML Query using the specified
+database table.
+
+  $sql = $query->sql( $table )
+
+Returns undef if the query could not be formed.
+
+=cut
+
+sub sql {
+  my $self = shift;
+
+  throw OMP::Error::DBMalformedQuery("sql method invoked with incorrect number of arguments\n")
+    unless scalar(@_) == 1;
+
+  my ($fbtable) = @_;
+
+  # Generate the WHERE clause from the query hash
+  # Note that we ignore elevation, airmass and date since
+  # these can not be dealt with in the database at the present
+  # time [they are used to calculate source availability]
+  # Disabling constraints on queries should be left to this
+  # subclass
+  my $subsql = $self->_qhash_tosql();
+  # Construct the the where clause. Depends on which
+  # additional queries are defined
+  my @where = grep { $_ } ( $subsql);
+  my $where = '';
+  $where = " WHERE " . join( " AND ", @where)
+    if @where;
+
+  # Now need to put this SQL into the template query
+  # This returns a row per response
+  # So will duplicate static fault info
+  my $sql = "(SELECT * FROM $fbtable $where)";
+
+  return "$sql\n";
+
+}
+
+=back
+
+=begin __PRIVATE__METHODS__
+
+=over 4
+
+=item B<_root_element>
+
+Class method that returns the name of the XML root element to be
+located in the query XML. Returns "ShiftQuery" by default.
+
+=cut
+
+sub _root_element {
+  return "FBQuery";
+}
+
+=back
+
+=end __PRIVATE__METHODS__
+
+=head1 AUTHOR
+
+Kynan Delorey E<lt>k.delorey@jach.hawaii.eduE<gt>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2002 Particle Physics and Astronomy Research Council.
+All Rights Reserved.
+
+=cut
+
+1;
