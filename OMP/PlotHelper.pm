ca7e4afb3811f154ccc816d948016318cf30412b
Kynan Delorey <k.delorey@jach.hawaii.edu>
2004-10-02 02:34:50 +0000

    + added internal method _get_bin_size + added support for a bin array argument in method bin_up + bin_up now returns an array of coordinates, rather that just   an array of Y values.
    svn path=/trunk/omp/perl/; revision=7739

--- OMP/PlotHelper.pm
+++ OMP/PlotHelper.pm
@@ -52,6 +52,9 @@ Bin up values by an arbitrary number (can be fractional).
 Arguments should be provided in hash form.  The arguments are:
 
   size     - Number to bin up by, or an array reference of bin sizes.
+             If an array reference is given, values are binned until
+             there are no more bins left, otherwise values are binned
+             until there are no more values left.
   method   - Method to use when binning.  A string that is either
              'sum' or 'average'.
   values   - An array reference whose elements are coordinates where the
@@ -61,7 +64,7 @@ Arguments should be provided in hash form.  The arguments are:
              value in the 'values' array.
 
 Except for 'startnum', all arguments are required. Returns an array
-where the elements are the value of each bin.
+of coordinates.
 
 =cut
 
@@ -74,8 +77,8 @@ sub bin_up {
   my @values = @{$args{values}};
 
   # Check that bin size is greater than 0
-  throw OMP::Error::BadArgs("Argument 'size' must be greater than 0")
-    unless ($size > 0);
+  throw OMP::Error::BadArgs("Argument 'size' must be either a number or an array reference")
+    unless (looks_like_number $size or ref($size) eq 'ARRAY');
 
   # Check that method argument is either 'sum' or 'average'
   throw OMP::Error::BadArgs("Argument 'method' must be either 'sum' or 'average'")
@@ -103,35 +106,66 @@ sub bin_up {
   # Get the high and low values for the bin.  Low value will be
   # the lowest x value in the array, unless a 'startnum' was provided,
   # in which case the low value will be the 'startnum' value.
+  my $bin_size = $self->_get_bin_size($size, 0);
   my $low_val = (defined $start_num ? $start_num : $values[0]->[0]);
-  my $high_val = $low_val + $size;
+  my $high_val = $low_val + $bin_size;
+
+  # The starting X value (middle of bin)
+  my $bin_mid = $low_val + ($bin_size / 2);
 
   # Bin up the values
   my @result;
   my $bin_num = 0; # Keep track of bin number
-  my $count = 0; # Keep count of values added to the bin
+  my @count; # Keep count of values added to each bin
   for my $val (@values) {
 
     until ($val->[0] >= $low_val and $val->[0] <= $high_val) {
-
-      # Value was outside the range of this bin.  Keep moving
+      # Value is outside the range of this bin.  Keep moving
       # through the bins until we find one that the value belongs in
-      $result[$bin_num] = 0
-	unless (defined $result[$bin_num]); # Set bin value to zero if nothing
-                                            # went in this bin.
-      $bin_num++; # Move to the next bin
-      $low_val += $size;
-      $high_val += $size;
-      $count = 0; # Reset the bin value counter
-    };
 
-    # Found a bin that the value belongs
-    # Add the y value to the bin
-    $result[$bin_num] += $val->[1];
-    $count++;
+      $result[$bin_num]->[1] = 0                 # Set bin Y value to zero if nothing
+	unless (defined $result[$bin_num]->[1]); # went in this bin.
 
-    # See if next value can go in this bin too
+      $result[$bin_num]->[0] = $bin_mid          # Set the X value if it wasn't set
+	unless (defined $result[$bin_num]->[0]); # earlier
+
+      # Move to the next bin
+      $bin_size = $self->_get_bin_size($size, ++$bin_num); # Increase bin number
+
+      if (defined $bin_size) {
+	$bin_mid += $bin_size / 2;
+	$low_val += $bin_size;
+	$high_val += $bin_size;
+      } else {
+	# $bin_size was undefined - we're out of bins, so stop.
+	last;
+      }
+    }
+
+    if (defined $bin_size) {
+      # Set the X value
+      $result[$bin_num]->[0] = $bin_mid
+	unless (defined $result[$bin_num]->[0]);
+
+      # Found a bin that the value belongs in
+      # Add the y value to the bin
+      $result[$bin_num]->[1] += $val->[1];
+      $count[$bin_num]++;
+
+    } else {
+      # Stop if we're out of bins
+      last;
+    }
   }
+
+  # Average the results if that method is being used
+  if ($method eq 'average') {
+    for (my $i = 0; $i <= $#result; $i++) {
+      $result[$i]->[1] = $result[$i]->[1] / $count[$i]
+	unless ($result[$i]->[1] == 0);
+    }
+  }
+
   return @result;
 }
 
@@ -199,6 +233,35 @@ sub bin_up_by_date {
 		       method => $method,
 		       startnum => $start_date,
 		       values => \@values,);
+}
+
+=back
+
+=head2 Internal Methods
+
+=over 4
+
+=item B<_get_bin_size>
+
+Get the size of a bin.  First argumentt should be either a bin
+size or an array reference containing bin sizes.  The second
+argument is the array index of the bin.  If the first argument
+is an array reference, the second argument is required.
+Returns undef if the bin size could not be obtained.
+
+=cut
+
+sub _get_bin_size {
+  my $self = shift;
+  my $bin = shift;
+  my $index = shift;
+
+  my $size = (ref($bin) eq 'ARRAY' ? $bin->[$index] : $bin);
+
+  throw OMP::Error::BadArgs("Bin size must be greater than 0")
+    unless ($size > 0 or ! defined $size);
+
+  return $size;
 }
 
 =back
