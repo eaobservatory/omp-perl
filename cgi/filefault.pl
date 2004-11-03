02646ab5a304a8d653346276c48007fcf2accf9d
Kynan Delorey <k.delorey@jach.hawaii.edu>
2004-11-03 23:43:19 +0000

    + Now uses OMP::CGIPage::Fault class
    svn path=/trunk/omp/perl/; revision=7930

--- cgi/filefault.pl
+++ cgi/filefault.pl
@@ -14,14 +14,13 @@ BEGIN {
 }
 
 # Load OMP modules
-use OMP::CGI;
-use OMP::CGI::FaultPage;
+use OMP::CGIPage::Fault;
 use OMP::General;
 
 my $arg = shift @ARGV;
 
 my $q = new CGI;
-my $cgi = new OMP::CGI( CGI => $q );
+my $cgi = new OMP::CGIPage::Fault( CGI => $q );
 
 # Set our theme
 my $theme = new HTML::WWWTheme("/WWW/omp-private/LookAndFeelConfig");
@@ -32,9 +31,9 @@ $cgi->html_title("OMP Fault System: File Fault");
 # If the user is outside the JAC network write the page with
 # authentication
 if (OMP::General->is_host_local) {
-  $cgi->write_page_fault( \&OMP::CGI::FaultPage::file_fault,
-			  \&OMP::CGI::FaultPage::file_fault_output);
+  $cgi->write_page('file_fault',
+		   'file_fault_output');
 } else {
-  $cgi->write_page_fault_auth( \&OMP::CGI::FaultPage::file_fault,
-			       \&OMP::CGI::FaultPage::file_fault_output);
+  $cgi->write_page_staff('file_fault',
+			 'file_fault_output');
 }
