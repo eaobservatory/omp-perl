a46a236a61e4d7e7eaef9857398966978c8212af
Kynan Delorey <k.delorey@jach.hawaii.edu>
2004-12-15 02:17:32 +0000

    + Fixed location of flex program description page in flexpage routine.
    svn path=/trunk/omp/perl/; revision=8199

--- OMP/CGIComponent/Helper.pm
+++ OMP/CGIComponent/Helper.pm
@@ -75,7 +75,7 @@ sub flex_page {
   }
 
   $sem = lc($sem);
-  my $flexpage = "/web/UKIRT/observing/omp/$sem/Flex_programme_descriptions.html";
+  my $flexpage = "/web/html/UKIRT/observing/omp/$sem/Flex_programme_descriptions.html";
 
   # Read in flex page
   open(FLEX, $flexpage) or die "Unable to open flex page [$flexpage]: $!";
