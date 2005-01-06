495eda046084fb20ab454637113438ff37408c52
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-01-06 08:50:38 +0000

    Add units support. See also JAC::OCS::Config::Interval
    svn path=/trunk/omp/perl/; revision=8319

--- OMP/Range.pm
+++ OMP/Range.pm
@@ -8,54 +8,108 @@ OMP::Range - Implement simple ranges
 
   use OMP::Range;
 
-  $r = new OMP::Range( Min => -4, Max => 20);
+  $r = new OMP::Range( Min => -4, Max => 20, units => 'pixels');
   $r = new OMP::Range( Min => 0 );
+  $r->units("pixels");
 
   print "$r";
 
 =head1 DESCRIPTION
 
-Simple class to implement a closed or open range. Exists mainly
-to allow stringification override.
-
-Ranges can be bound or unbound. If C<max> is less than C<min>
-the range is inverted.
+Simple class to implement a closed or open range. This is a subclass
+of C<Number::Interval> with the addition of support for an attribute
+specifying the units of the numbers used to specify the range.
 
 =cut
 
 use strict;
 use warnings;
+use Carp;
 
-use Number::Interval;
 use base qw/ Number::Interval /;
+use vars qw/ $VERSION /;
 
-=head1 NOTES
+$VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
 
-A small wrapper class around the C<Number::Interval> module.
+=head1 METHODS
 
-=head1 COPYRIGHT
+=head2 Constructor
 
-Copyright (C) 2002-2004 Particle Physics and Astronomy Research Council.
-All Rights Reserved.
+=over 4
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or (at
-your option) any later version.
+=item B<new>
 
-This program is distributed in the hope that it will be useful,but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
+Create a new object. Can be populated when supplied with
+keys C<Max> and C<Min>.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA 02111-1307,
-USA
+  $r = new JAC::OCS::Config::Range();
+  $r = new JAC::OCS::Config::Range( Max => 5, Units => 'pixels' );
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $i = $class->SUPER::new( @_ );
+
+  # Populate it with units
+  if (@_) {
+    my %args = @_;
+    $i->units( $args{Units}) if exists $args{Units};
+  }
+
+  return $i;
+}
+
+=back
+
+=head2 Accessors
+
+=over 4
+
+=item B<units>
+
+Units of the numbers comprising the interval.
+
+  $u = $r->units;
+  $i->units("pixels");
+
+Can return C<undef> if no unit has been specified.
+
+=cut
+
+sub units {
+  my $self = shift;
+  if (@_) {
+    $self->{Units} = shift;
+  }
+  return $self->{Units};
+}
+
+=back
+
+=head1 SEE ALSO
+
+L<Number::Interval>
 
 =head1 AUTHOR
 
-Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>.
+Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
+
+Copyright 2004-2005 Particle Physics and Astronomy Research Council.
+All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
 
 =cut
 
