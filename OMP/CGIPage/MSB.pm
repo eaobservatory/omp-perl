e5d9d30ea1b0b14cb74df07affbb1fe0a9c81ddb
Kynan Delorey <k.delorey@jach.hawaii.edu>
2005-01-25 00:10:45 +0000

    Changes to observed output:   + Now displays all MSB comments associated with an MSB (so now you can see your     comment after you've submitted it)   + Reorganized logic   + Now re-runs MSB comment query and displays comments after a comment is     submitted
    svn path=/trunk/omp/perl/; revision=8383

--- OMP/CGIPage/MSB.pm
+++ OMP/CGIPage/MSB.pm
@@ -247,22 +247,60 @@ sub observed_output {
   if ($q->param("Add Comment")) {
     print $q->h2("Add a comment to MSB");
     OMP::CGIComponent::MSB::msb_comment_form($q);
-  }
+  } elsif ($q->param("Remove")) {
+    # Clicked Remove button
+    try {
+      OMP::MSBServer->alldoneMSB( $q->param('projectid'), $q->param('checksum'));
+      print $q->h2("MSB removed from consideration");
+    } catch OMP::Error::MSBMissing with {
+      print "MSB not found in database";
+    } otherwise {
+      my $Error = shift;
+      print "An error occurred while attempting to mark the MSB as Done: $Error";
+    };
+  } elsif ($q->param("Submit")) {
+    # Submitted a comment
 
-  if (!$q->param("Add Comment")) {
+    try {
+      # Get the user object
+      my $user = OMP::UserServer->getUser($q->param('author')) or
+	throw OMP::Error::BadArgs( "Must supply a valid OMP user ID");
+	
+      # Create the comment object
+      my $comment = new OMP::Info::Comment( author => $user,
+					    text => $q->param('comment'),
+					    status => OMP__DONE_COMMENT );
+	
+      OMP::MSBServer->addMSBcomment( $q->param('projectid'), $q->param('msbid'), $comment);
+
+      print $q->h2("MSB comment successfully submitted");
+    } catch OMP::Error::MSBMissing with {
+      print "MSB not found in database";
+    } otherwise {
+      my $Error = shift;
+      print "An error occurred while attempting to submit the comment: $Error";
+    };
+  } else {
     # Just viewing comments
     OMP::CGIComponent::MSB::observed_form($q);
-    print $q->hr;
+  }
 
+  print $q->hr;
+
+  if (! $q->param("Add Comment")) {
     my $utdate = $q->param('utdate');
-
     my $telescope = $q->param('telescope');
 
-    my $dbconnection = new OMP::DBbackend;
+    # Do nothing if telescope not selected
+    if (! $telescope) {
+      print $q->h2("Please select a telescope");
+      return;
+    }
 
+    my $dbconnection = new OMP::DBbackend;
     my $commentref = OMP::MSBServer->observedMSBs({date => $utdate,
-						  returnall => 0,
-						  format => 'data'});
+						   returnall => 1,
+						   format => 'data'});
 
     # Now keep only the comments that are for the telescope we want
     # to see observed msbs for
@@ -281,45 +319,6 @@ sub observed_output {
       or print $q->h2("No MSBs observed on $utdate");
 
     OMP::CGIComponent::MSB::msb_comments_by_project($q, \@msbs);
-
-    # If they've just submitted a comment show some comforting output
-    # or catch an error
-    if ($q->param("Submit")) {
-      try {
-	# Get the user object
-	my $user = OMP::UserServer->getUser($q->param('author')) or
-	  throw OMP::Error::BadArgs( "Must supply a valid OMP user ID");
-	
-	# Create the comment object
-	my $comment = new OMP::Info::Comment( author => $user,
-					      text => $q->param('comment'),
-					      status => OMP__DONE_COMMENT );
-	
-	OMP::MSBServer->addMSBcomment( $q->param('projectid'), $q->param('msbid'), $comment);
-	
-	print $q->h2("MSB comment successfully submitted");
-      } catch OMP::Error::MSBMissing with {
-	print "MSB not found in database";
-      } otherwise {
-	my $Error = shift;
-	print "An error occurred while attempting to submit the comment: $Error";
-      };
-    }
-    
-    # If they click the "Mark as Done" button mark it as done
-    
-    if ($q->param("Remove")) {
-      try {
-	OMP::MSBServer->alldoneMSB( $q->param('projectid'), $q->param('checksum'));
-	print $q->h2("MSB removed from consideration");
-      } catch OMP::Error::MSBMissing with {
-	print "MSB not found in database";
-      } otherwise {
-	my $Error = shift;
-      print "An error occurred while attempting to mark the MSB as Done: $Error";
-    };
-    }
-    
     (@msbs) and OMP::CGIComponent::MSB::observed_form($q);
   }
 }
