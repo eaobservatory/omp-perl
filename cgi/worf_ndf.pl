b2ae80049d4df153d4c908a5401928beaeddc754
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2005-02-22 23:33:41 +0000

    initial addition
    svn path=/trunk/omp/perl/; revision=8539

---
+++ cgi/worf_ndf.pl
@@ -0,0 +1,43 @@
+#!/local/perl/bin/perl -XT
+#
+# WWW Observing Remotely Facility (WORF)
+#
+# http://www.jach.hawaii.edu/JACpublic/UKIRT/software/worf/
+#
+# Requirements:
+# TBD
+#
+# Authors: Frossie Economou (f.economou@jach.hawaii.edu)
+#          Brad Cavanagh (b.cavanagh@jach.hawaii.edu)
+
+use strict;
+
+# Standard initialisation (not much shorter than the previous
+# code but no longer has the module path hard-coded)
+BEGIN {
+  my $retval = do "./omp-cgi-init.pl";
+  unless ($retval) {
+    warn "couldn't parse omp-cgi-init.pl: $@" if $@;
+    warn "couldn't do omp-cgi-init.pl: $!"    unless defined $retval;
+    warn "couldn't run omp-cgi-init.pl"       unless $retval;
+    exit;
+  }
+}
+
+# Set up the environment for data conversion
+
+BEGIN {
+  $ENV{'HDS_SCRATCH'} = "/tmp";
+}
+
+# OMP Modules
+use OMP::CGIComponent::WORF;
+
+use strict;
+
+# Set up global variables, system variables, etc.
+
+my $query = new CGI;
+
+return_ndf( $query );
+
