667aab4ca0a1d5f13fc1ae49bdd11e8073077251
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-03-15 22:51:43 +0000

    add GPL to pod
    svn path=/trunk/omp/perl/; revision=8698

--- OMP/CGIPage/Obslog.pm
+++ OMP/CGIPage/Obslog.pm
@@ -419,6 +419,21 @@ Kynan Delorey E<lt>k.delorey@jach.hawaii.eduE<gt>
 Copyright (C) 2002 Particle Physics and Astronomy Research Council.
 All Rights Reserved.
 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program (see SLA_CONDITIONS); if not, write to the
+Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+Boston, MA  02111-1307  USA
+
 =cut
 
 1;
