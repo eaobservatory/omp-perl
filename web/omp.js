9653aca2f7030d18574301cb0c803b9c985c169c
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-03-16 00:47:17 +0000

    add GPL
    svn path=/trunk/omp/perl/; revision=8701

--- web/omp.js
+++ web/omp.js
@@ -1,3 +1,36 @@
+/*
+
+Name:
+  omp.js  
+
+Description:
+  Javascript functions used by the OMP web pages.
+
+Author:
+  Kynan Delorey <k.delorey@jach.hawaii.edu>
+
+Copyright:
+  Copyright (C) 2004-2005 Particle Physics and Astronomy Research Council.
+  All Rights Reserved.
+
+License:
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA 02111-1307,
+  USA
+
+*/
+
 // Toggle the display of rows of a particular system and status
 // If given a single argument, that argument should be the system
 // of the rows to be hidden.  If more than one argument is given,
