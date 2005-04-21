1f0f8a71d9277942a220b30b4d3f16b01d0148a3
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-04-21 08:11:54 +0000

    + Add -sim option + Add -transdir option + Read from arg list as well as from stdin
    svn path=/trunk/omp/perl/; revision=8884

--- client/jcmttranslator.pl
+++ client/jcmttranslator.pl
@@ -8,17 +8,18 @@ jcmttranslator - translate science program XML to ODFs
 
   cat sp.xml | jcmttranslator > outfile
 
+  jcmttranslator -sim msb.xml
+
 =head1 DESCRIPTION
 
-Reads science program xml from standard input and writes the
-translated odf file name to standard output. The filename is
-suitable for reading into the new DRAMA Queue.
+Reads science program xml either from standard input or by filename and writes the
+translated configuration to disk. The name of the file that should be loaded into
+the queue is written to standard output.
 
 =head1 ARGUMENTS
 
-The following arguments are allowed:
-
-NONE
+The input science program can either be supplied as a filename argument or by
+piping the XML from standard input. A supplied filename supercedes standard input.
 
 =head1 OPTIONS
 
@@ -43,10 +44,20 @@ the translated config class (ie a single HTML file for DAS translations,
 a SCUBA ODF macro for SCUBA). Note that if this switch is enabled,
 mixed instrument MSBs are not supported.
 
+=item B<-sim>
+
+Enables simulation mode for translators that support this.
+
+=item B<-transdir>
+
+Specify an explicit directory to receive the translated output files.
+Supercedes the C<-cwd> option if both are specified.
+
 =item B<-cwd>
 
 Write the translated files to the current working directory
 rather than to the standard translation directory location.
+C<-transdir> supercedes C<-cwd> if both are specified.
 
 =item B<-debug>
 
@@ -73,13 +84,17 @@ use OMP::TransServer;
 use File::Spec;
 
 # Options
-my ($help, $man, $debug, $cwd, $old);
+my ($help, $man, $debug, $cwd, $old, $sim, $transdir);
 my $status = GetOptions("help" => \$help,
 			"man" => \$man,
 			"debug" => \$debug,
 			"cwd" => \$cwd,
+			"sim" => \$sim,
 			"old" => \$old,
+			"transdir=s" => \$transdir,
 		       );
+
+pod2usage(1) if !$status;
 
 pod2usage(1) if $help;
 pod2usage(-exitstatus => 0, -verbose => 2) if $man;
@@ -88,22 +103,45 @@ pod2usage(-exitstatus => 0, -verbose => 2) if $man;
 $OMP::Translator::DEBUG = 1 if $debug;
 
 # Translation directory override
-OMP::Translator->outputdir(".") if $cwd;
+if ($transdir) {
+  OMP::Translator->outputdir($transdir);
+} elsif ($cwd) {
+  OMP::Translator->outputdir(".");
+}
+
 
 # Configure translation mode
 OMP::Translator->backwards_compatibility_mode( 1 ) if $old;
 
 # Now for the action
-# Read from standard input
+# Read from standard input or from the command line.
 my $xml;
 {
   # Can not let this localization propoagate to the OMP classes
   # since this affects the srccatalog parsing
   local $/ = undef;
-  $xml = <>;
+
+  if (@ARGV) {
+    my $file = shift(@ARGV);
+    open my $fh, "< $file" or die "Error reading input science program from file $file: $!";
+    $xml = <$fh>;
+  } else {
+    # Stdin should be readable
+    my $rin = '';
+    vec($rin,fileno(STDIN),1) = 1;
+    my $nfound = select($rin,undef,undef,0.1);
+
+    if ($nfound) {
+      $xml = <>;
+    } else {
+      die "No filename specified for science program and nothing appearing from pipe on STDIN\n";
+    }
+
+  }
+
 }
 
-my $filename = OMP::TransServer->translate( $xml );
+my $filename = OMP::TransServer->translate( $xml, { simulate => $sim });
 
 # convert the filename to an absolute path
 print File::Spec->rel2abs($filename) ."\n";
