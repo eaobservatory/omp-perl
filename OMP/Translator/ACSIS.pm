40bccbb4acef1beccdf63650f720f35e24f19a59
Jamie Leech <j.leech@jach.hawaii.edu>
2005-12-22 02:16:31 +0000

    Changed all HARP layouts to s8r8g1 for simplicity. Maye need to changed back when testing fast rasters etc.
    svn path=/trunk/omp/perl/; revision=9493

--- OMP/Translator/ACSIS.pm
+++ OMP/Translator/ACSIS.pm
@@ -80,7 +80,7 @@ my %ACSIS_Layouts = (
 		    RXA => 's1r1g1',
 		    RXB => 's2r2g1',
 		    RXW => 's2r2g2',
-		    HARPB => 's8r16g8'
+		    HARPB => 's8r8g1'
 		   );
 
 # LO2 synthesizer step, hard-wired
