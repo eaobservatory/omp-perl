d644da6700544818c47b029c6d299774477799aa
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-01-10 00:37:13 +0000

    Fix copyright year
    svn path=/trunk/omp/perl/; revision=9522

--- OMP/MSB.pm
+++ OMP/MSB.pm
@@ -4831,7 +4831,7 @@ Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001-2003 Particle Physics and Astronomy Research Council.
+Copyright (C) 2001-2006 Particle Physics and Astronomy Research Council.
 All Rights Reserved.
 
 This program is free software; you can redistribute it and/or modify
