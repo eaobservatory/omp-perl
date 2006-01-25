215758925a3228ffc433ebc76dfb158411e81594
Kynan Delorey <k.delorey@jach.hawaii.edu>
2006-01-25 02:12:27 +0000

    + Project title and PI name now appear in the title for the page (in the browser's title bar).
    svn path=/trunk/omp/perl/; revision=9544

--- cgi/projecthome.pl
+++ cgi/projecthome.pl
@@ -16,13 +16,28 @@ BEGIN {
 # Load OMP modules
 use OMP::CGIPage;
 use OMP::CGIPage::Project;
+use OMP::ProjServer;
 
 my $arg = shift @ARGV;
 
 my $q = new CGI;
 my $cgi = new OMP::CGIPage( CGI => $q );
 
-my $title = $cgi->html_title;
-$cgi->html_title("$title: Project home");
+my $title = $cgi->html_title . ": Project home";
+my $projid = uc($cgi->_get_param('projectid'));
+my $password = $cgi->_get_param('password');
+my $verify;
+if ($projid and $password) {
+  $verify = OMP::ProjServer->verifyPassword($projid, $password);
+}
+
+if ($verify) {
+  my $projobj = OMP::ProjServer->projectDetails($projid, $password, "object");
+  my $projpi = $projobj->pi;
+
+  $title = "$projid ($projpi) - ". $cgi->html_title;
+}
+
+$cgi->html_title($title);
 $cgi->write_page( \&OMP::CGIPage::Project::project_home,
 		  \&OMP::CGIPage::Project::project_home );
