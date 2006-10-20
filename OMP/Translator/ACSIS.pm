bc9de12b0b698d2bcbcfef40875d7aabbd87f756
Remo Tilanus <r.tilanus@jach.hawaii.edu>
2006-10-20 23:12:01 +0000

    Default: nod_set_size=4 iso 2 (erroneously left at 2 after switch-back to ABBA
    svn path=/trunk/omp/perl/; revision=10546

--- OMP/Translator/ACSIS.pm
+++ OMP/Translator/ACSIS.pm
@@ -3411,7 +3411,7 @@ sub get_nod_set_size {
 
   my $nod_set_size;
   if (!defined $info{nodSetDefinition}) {
-    $nod_set_size = 2; "ABBA";
+    $nod_set_size = 4; "ABBA";
   } elsif ($info{nodSetDefinition} eq 'AB') {
     $nod_set_size = 2;
   } elsif ($info{nodSetDefinition} eq 'ABBA') {
