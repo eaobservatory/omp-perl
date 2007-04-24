903fb6271ce2762d2680ce66ab641665b73accae
Tim Jenness <t.jenness@jach.hawaii.edu>
2007-04-24 23:42:17 +0000

    Add JLS and UKIRT UKIDSS Backup and Campaign program recognition
    svn path=/trunk/omp/perl/; revision=11147

--- OMP/General.pm
+++ OMP/General.pm
@@ -1228,10 +1228,13 @@ sub extract_projectid {
       or $string =~ /\b([ms]\d\d[ab][unchid]\d+([a-z]|fb)?)\b/i # JCMT [inc serv, FB and A/B suffix]
       or $string =~ /\b(m\d\d[ab]ec\d+)\b/i         # JCMT E&C
       or $string =~ /\b(m\d\d[ab]gt\d+)\b/i         # JCMT Guaranteed Time
+      or $string =~ /\b(mjls[sgnc]\d+)\b/i          # JCMT Legacy Surveys
       or $string =~ /\b(m\d\d[ab]h\d+[a-z]\d?)\b/i  # UH funny suffix JCMT
       or $string =~ /\b(u\/serv\/\d+)\b/i           # UKIRT serv
       or $string =~ /\b(u\/ec\/\d+)\b/i             # UKIRT E&C
       or $string =~ /\b(u\/ukidss\/[a-z]{3}(\d+[a-z]?|_sv)?)\b/i # UKIRT UKIDSS program
+      or $string =~ /\b(u\/ukidss\/\d+)\b/i         # UKIRT Backup UKIDSS programs
+      or $string =~ /\b(u\/cmp\/\d+)\b/i            # UKIRT Campaigns
       or $string =~ /\b(nls\d+)\b/i                 # JCMT Dutch service (deprecated format)
       or $string =~ /\b([LS]X_\d\d\w\w_\w\w)\b/i    # SHADES proposal
       or $string =~ /\b([A-Za-z]+CAL)\b/i           # Things like JCMTCAL
