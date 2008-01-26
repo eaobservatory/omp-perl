f349714c5b7f2a572ae3cba7cbefdbac24a11597
Tim Jenness <t.jenness@jach.hawaii.edu>
2008-01-26 01:33:42 +0000

    + Factor out header merging and Obs creation into separate methods so that   both file and DB lookup can share code + DB lookup for JCMT COMMON now includes filename information + OBSIDs are used for uniqueness test (requires Astro::FITS::HdrTrans patch to support OBSID) + Remove TEMP_INST and BOLOMS hacks (Astro::FITS::HdrTrans now cleverer)
    svn path=/trunk/omp/perl/; revision=14813

--- OMP/ArchiveDB.pm
+++ OMP/ArchiveDB.pm
@@ -47,6 +47,7 @@ use SCUBA::ODF;
 
 use vars qw/ $VERSION $FallbackToFiles $SkipDBLookup /;
 
+use Scalar::Util qw/ blessed /;
 use Data::Dumper;
 use base qw/ OMP::BaseDB /;
 
@@ -379,7 +380,6 @@ sub _query_files {
   my $obsgroup;
   my @files;
   my @obs;
-  my %headers;
 
   # If we have a simple query, go to the cache for stored information and files
   if( simple_query( $query ) ) {
@@ -421,6 +421,7 @@ sub _query_files {
     } # for( my $day... )
   } # if( simple_query( $query ) ) { } else
 
+  my @allheaders;
   foreach my $arr_ref ( @files ) {
 
     foreach my $file ( @$arr_ref ) {
@@ -472,18 +473,12 @@ sub _query_files {
           throw OMP::Error::FatalError( "Do not recognize file suffix for file $file. Cannot read header" );
         }
 
-        # Translate header.
-        tie my %header, ref( $FITS_header ), $FITS_header;
-        my %generic_header = Astro::FITS::HdrTrans::translate_from_FITS( \%header,
-                                                                         frameset => $frameset );
+        # Push onto array so that we can filter by OBSID later
+        push(@allheaders, {
+                        header => $FITS_header,
+                        filename => $file,
+                        frameset => $frameset } );
 
-        # If this OBSERVATION_ID has been seen before, merge the
-        # headers. Otherwise, just store the header.
-        my $obsid = $generic_header{'OBSERVATION_ID'};
-
-        push( @{$headers{$obsid}{'headers'}}, $FITS_header );
-        push( @{$headers{$obsid}{'filenames'}}, $file );
-        push( @{$headers{$obsid}{'framesets'}}, $frameset );
       }
       catch Error with {
         my $Error = shift;
@@ -496,33 +491,14 @@ sub _query_files {
     }
   }
 
-  foreach my $obsid ( keys %headers ) {
+  # merge duplicate information into a hash indexed by obsid
+  my %headers = $self->_merge_dupes( @allheaders );
 
-    # Merge all the headers for this given obsid, but only if we have
-    # more than one.
-    my $FITS_header;
-    if( $#{$headers{$obsid}{'headers'}} > 0 ) {
-      ( my $merged, my @different ) = ${$headers{$obsid}{'headers'}}[0]->merge_primary( { merge_unique => 1 }, @{$headers{$obsid}{'headers'}}[1..$#{$headers{$obsid}{'headers'}}] );
-      $merged->subhdrs( @different );
-      $FITS_header = $merged;
-    } else {
-      $FITS_header = $headers{$obsid}{'headers'}[0];
-    }
+  # and create obs objects
+  my @observations = $self->_hdrs_to_obs( $retainhdr, %headers);
 
-    my $frameset = $headers{$obsid}{'framesets'}[0];
-
-    # Create the Obs object.
-    my $obs;
-    my $Error;
-
-    $obs = new OMP::Info::Obs( fits => $FITS_header,
-                               retainhdr => $retainhdr,
-                               wcs => $frameset,
-                             );
-
-    if( !defined( $obs ) ) { next; }
-
-    $obs->filename( \@{$headers{$obsid}{'filenames'}} );
+  # Now filter
+  foreach my $obs ( @observations ) {
 
     # If the observation's time falls within the range, we'll create the object.
     my $match_date = 0;
@@ -624,75 +600,169 @@ sub _reorganize_archive {
     $retainhdr = 0;
   }
 
-  my @return;
-
-# For each row returned by the query, create an Info::Obs object
-# out of the information contained within.
+  # The first thing we have to do is merge related rows based on obsid.
+  # We assume that the query has given us a row per useful quantity each with an obsid somewhere
+  # first need to create a new array that matches requirement of _merge_dupes
+  # Header translation requires that we have upper case keys to match the FITS standard.
+  # Since _merge_dupes uses header translation we need to upper case everything at the same time.
+  my @rearranged;
   for my $row (@$rows) {
-
-    # Convert all the keys into upper-case.
-    my $newrow;
-    for my $key (keys %$row) {
-      my $uckey = uc($key);
-      $newrow->{$uckey} = $row->{$key};
+    # upper case
+    my %newrow;
+    for my $k (keys %$row) {
+      $newrow{uc($k)} = $row->{$k};
     }
-    # Find out which telescope we're dealing with. If it's UKIRT,
-    # then we have to run the UKIRTDB translation. If it's JCMT,
-    # we can just use the instrument-specific translation.
-    my $telescope = $newrow->{TELESCOP};
-    my $instrument = $newrow->{INSTRUME};
-    if(defined($telescope) && $telescope =~ /UKIRT/i) {
+    push(@rearranged, { header => \%newrow } );
+  }
+  my %unique = $self->_merge_dupes( @rearranged );
 
-      # We need to temporarily set the instrument header to
-      # 'UKIRTDB' so the correct translation can be used.
-      # Also set another temporary header that will never be
-      # set from the database (i.e. key name longer than 8 characters)
-      # so the translation code knows what instrument we have.
-      $newrow->{INSTRUME} = 'UKIRTDB';
-      $newrow->{TEMP_INST} = uc($instrument);
+  # now convert into Obs::Info objects
+  return $self->_hdrs_to_obs( $retainhdr, %unique);
+}
+
+=item B<_merge_dupes>
+
+Given an array of hashes containing header object, filename and frameset, merge into a hash indexed
+by OBSID where headers have been merged and filenames have been combined into arrays.
+
+  %merged = $arcdb->_merge_dupes( @unmerged );
+
+In the returned merged version, header hash item will contain an Astro::FITS::Header object
+if the supplied entry in @unmerged was a simple hash reference.
+
+Input keys:
+
+   header - reference to hash or an Astro::FITS::Header object
+   filename - single filename (optional)
+   frameset - optional Starlink::AST frameset
+
+If the 'filename' is not supplied yet a header translation reports that FILENAME
+is available, the value will be stored from the translation into the filename slot.
+
+Output keys
+
+   header - Astro::FITS::Header object
+   filenames - reference to array of filenames
+   frameset - optional Starlink::AST frameset (from first in list)
+
+=cut
+
+sub _merge_dupes {
+  my $self = shift;
+
+  # Take local copies so that we can add information without affecting caller
+  my @unmerged = map { {%$_} } @_;
+
+  my %unique;
+  for my $info ( @unmerged ) {
+
+    # First need to work out which item corresponds to the unique observation ID
+    # We do this via header translation. Since we can in principal have multiple
+    # instruments we have to redetermine the class each time round the loop.
+
+    # Convert fits header to hash if required
+    my $hdr = $info->{header};
+    if (blessed($hdr)) {
+      tie my %header, ref($hdr), $hdr;
+      $hdr = \%header;
+    } else {
+      # convert the header hash to a Astro::FITS::Header object so that we can easily
+      # merge it later
+      $info->{header} = Astro::FITS::Header->new( Hash => $hdr );
     }
+    my $class = Astro::FITS::HdrTrans::determine_class( $hdr, undef, 1);
 
-    # Hack to get SCUBA to be recognized.
-    if(defined($newrow->{BOLOMS})) {
-      $newrow->{INSTRUME} = 'SCUBA';
-      $instrument = "SCUBA";
-    }
+    my $obsid = $class->to_OBSERVATION_ID( $hdr, $info->{frameset} );
 
-    if(exists($newrow->{BACKEND}) || exists($newrow->{C1BKE}) ) {
-      if( uc($newrow->{BACKEND}) eq 'ACSIS' ) {
-        $instrument = 'ACSIS';
-      } else {
-        $instrument = (defined($newrow->{FRONTEND}) ? uc($newrow->{FRONTEND}) : uc($newrow->{C1RCV}) );
+    # check for translated filename
+    if (!exists $info->{filename}) {
+      my $filename = $class->to_FILENAME( $hdr );
+      if (defined $filename) {
+        $info->{filename} = $filename;
       }
     }
 
-    # Create an Info::Obs object.
-    my $obs = new OMP::Info::Obs( hdrhash => $newrow, retainhdr => $retainhdr );
-
-    # If the instrument wasn't heterodyne (RxA3, RxB3 or RxW) or ACSIS
-    # then set the instrument back to what it was before.
-    if( $instrument !~ /^rx/i && $instrument ne 'ACSIS' ) {
-      $obs->instrument( $instrument );
-    }
-
-    # Check the filename. If it's not set, call file_from_bits and set it.
-    if( !defined( $obs->filename ) ) {
-      $obs->filename( $obs->file_from_bits );
-    }
-
-    # And push it onto the @return array.
-    push @return, $obs;
+    # store it on hash indexed by obsid
+    $unique{$obsid} = [] unless exists $unique{$obsid};
+    push(@{$unique{$obsid}}, $info);
   }
 
-  # Strip out duplicates
-  my %seen = ();
-  my @uniq;
-  foreach my $obs (@return) {
-    push( @uniq, $obs ) unless $seen{$obs->instrument . $obs->runnr . $obs->startobs}++;
+  # Now go through again and merge the headers and filename information for multiple files
+  # but identical obsid.
+  for my $obsid (keys %unique) {
+    # To simplify syntax get array of headers and filenames
+    my (@fits, @files, $frameset);
+    for my $f (@{$unique{$obsid}}) {
+      push(@fits, $f->{header});
+      push(@files, $f->{filename});
+      $frameset = $f->{frameset} if defined $f->{frameset};
+    }
+
+    # Merge if necessary (shift off the primary)
+    my $fitshdr = shift(@fits);
+#    use Data::Dumper; print Dumper($fitshdr);
+    if (@fits) {
+      # need to merge
+#      print "FITS:$_\n" for @fits;
+      my ( $merged, @different ) = $fitshdr->merge_primary( { merge_unique => 1 }, @fits );
+#      print Dumper(\@different);
+
+      $merged->subhdrs( @different );
+#      print Dumper({merged => $merged, different => \@different});
+      $fitshdr = $merged;
+    }
+
+    # Some queries result in duplicate rows for filename so uniqify
+    my %files_uniq = map { $_ => undef } @files;
+    @files = keys %files_uniq;
+
+    $unique{$obsid} = {
+                       header => $fitshdr,
+                       filenames => \@files,
+                       frameset => $frameset,
+                      };
   }
 
-  return @uniq;
+  return %unique;
+}
 
+=item B<_hdrs_to_obs>
+
+Convert the result from _merge_dupes() method to an array of C<OMP::Info::Obs> objects.
+
+  @obs = $arcdb->_hdrs_to_obs( $retainhdr, %merged );
+
+=cut
+
+sub _hdrs_to_obs {
+  my $self = shift;
+  my $retainhdr = shift;
+  my %merged = @_;
+
+  my @observations;
+  foreach my $obsid ( keys %merged ) {
+
+    # Create the Obs object.
+    my $obs = new OMP::Info::Obs( fits => $merged{$obsid}{header},
+                                  retainhdr => $retainhdr,
+                                  wcs => $merged{$obsid}{frameset},
+                                );
+
+    if( !defined( $obs ) ) { print "Error creating obs $obsid\n";next; }
+
+    # store the filename information
+    $obs->filename( \@{$merged{$obsid}{'filenames'}}, 1 );
+
+    # Ask for the raw data directory
+    my $rawdir = $obs->rawdatadir;
+
+    
+
+
+    push(@observations, $obs);
+  }
+
+  return @observations;
 }
 
 =back
