c1268a5b423547c8a53fa1966ac7b33a6f040c22
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2008-04-03 08:24:53 +0000

    - Handle semesters -- as date range provided by OMP::General->semester_boundary   -- for a given telescope in addition to project ids.
      Now SQL is present in raw instead of XML (until that can be convereted to XML
      again).
    
    
    svn path=/trunk/omp/perl/; revision=15168

--- client/omp-time-acct.pl
+++ client/omp-time-acct.pl
@@ -2,6 +2,7 @@
 
 use warnings;
 use strict;
+use Data::Dumper;
 
 use Getopt::Long qw/ :config gnu_compat no_ignore_case /;
 use Pod::Usage;
@@ -16,120 +17,222 @@ BEGIN {
   $ENV{'OMP_DIR'} = OMPLIB unless exists $ENV{'OMP_DIR'};
 }
 
+use OMP::Constants qw/ :logging /;
 use OMP::General;
+use OMP::BaseDB;
 use OMP::DBbackend;
-use OMP::TimeAcctDB;
-use OMP::TimeAcctQuery;
 
 my %default_opt =
-  ( 'header' => undef,
-    'divider' => undef,
-    'file' => [],
-    'timezone' => 'UTC',
-    'start-date' => undef,
-    'end-date' => undef,
-    # Individual dates.
-    'date' => []
+  ( 'header'   => undef,
+    'projects' => [],
+    # Date-time ranges (array reference of hash references, with 'min' & 'max'
+    # as date-time keys).
+    'date-range' => [],
+    'telescope'  => undef
   );
-
 my %opt = process_options( %default_opt );
 
-my @proj = get_projects( \%opt, @ARGV )
-  or pod2usage( '-exitval' => 2, '-verbose' => 99, '-sections' => 'SYNOPSIS',
-                '-msg' => 'No project ids were given.'
-              );
-my $last = $proj[-1];
+my $db =
+  OMP::BaseDB->new( 'DB' => OMP::DBbackend->new )->_dbhandle
+    or die "Failed to acquire a database handle.\n";
 
-my $db = OMP::TimeAcctDB->new( 'DB' => OMP::DBbackend->new );
+my $query = make_project_query_2( @opt{qw/ projects date-range /} );
+OMP::General->log_message( "$0: Query: $query", OMP__LOG_INFO );
 
-for my $id ( @proj ) {
+my $st = $db->prepare( $query ) or die log_db_error( $db->errstr );
 
-  print_header( $opt{'header'} );
+$st->execute or die log_db_error( $db->errstr );
 
-  print_csv( $db->queryTimeSpent( make_query( $id ) ) );
+{
+  my $print_head;
+  while ( my @rec = $st->fetchrow_array ) {
 
-  print "\n" if $opt{'divider'} && $id ne $last;
+    #  Print header only if there are any results, so it is here in loop.
+    !$print_head and
+      $print_head = 1, print_header() if $opt{'header'};
+
+    print_csv( @rec );
+  }
 }
 
 exit;
 
-sub parse_date {
-
-  my ( $date ) = @_;
-
-  my $parsed;
-
-
-  return $parsed;
-}
-
 BEGIN {
 
-  # The method names to be called, in order, on given C<OMP::TimeAcctQuery>
-  # object.
+  # The column names (and possible method names to be called, in order, on given
+  # C<OMP::TimeAcctQuery> object).
   my @header = qw{ projectid timespent date confirmed } ;
 
-  #  Prints CSV given a list of C<OMP::TimeAcctQuery> objects.
+  #  Prints CSV given a list of elements of a record.
   sub print_csv {
 
-    my ( @acct ) = @_;
+    my ( @rec ) = @_;
 
-    for my $ac ( @acct ) {
+    # Special handling of date.
+    for ( $rec[2] ) {
 
-      my @val = map { $ac->$_ } @header ;
-      # Specail handling of date.
-      for ( $val[2] ) {
-
-        $_ = OMP::General->parse_date( $_ ) || '';
-        $_ = $_->ymd if blessed( $_ ) =~ m/^Time::Piece\b/;
-      }
-      really_print_csv( @val );
+      $_ = OMP::General->parse_date( $_ ) || '';
+      $_ = $_->ymd if blessed( $_ ) =~ m/^Time::Piece\b/;
     }
-    return;
+
+    return really_print_csv( @rec );
   }
 
   # Prints the header given a flag (if & how many times to print) and list of
   # header strings.
-  sub print_header { return really_print_header( $_[0], @header ); }
+  sub print_header { return really_print_header( @header ); }
 }
 
-{
-  my $once;
-  # Prints header as CSV given a flag and a list of header strings.  Flag
-  # specifies if to print the header at all, only once, or every time.
-  sub really_print_header {
+sub really_print_header {
 
-    my $header_once = shift;
+  my @header = @_;
 
-    return unless $header_once;
-    return if $once && $header_once == 1;
-
-    $once++;
-    my @header = @_;
-    return
-      # Header in lower case after normalizing any camelCase ones.
-      really_print_csv( map { s/([a-z])([A-Z])/$1_$2/; lc $_ } @header );
-  }
+  return
+    # Header in lower case after normalizing any camelCase ones.
+    really_print_csv( map { s/([a-z])([A-Z])/$1_$2/; lc $_ } @header );
 }
 
-# Simple mindedly prints CSV (see I<Text::CSV> for robustness).
+# Simple minded-ly prints CSV (see I<Text::CSV> for robustness).
 sub really_print_csv {
 
-  printf "%s\n", join ',', @_;
-  return;
+  printf "%s\n", join ',', @_; return;
 }
 
-# Returns project id list given the options hash reference, and any additional
-# project ids.
+sub make_project_query_2 {
+
+  my ( $projs, $ranges ) = @_;
+
+  unless ( scalar @{ $projs } || scalar @{ $ranges } ) {
+
+    warn "Neither project ids given, nor were date ranges with telescope.";
+    return;
+  }
+
+  my $sql = '';
+  if ( scalar @{ $ranges } ) {
+
+    $sql =
+      join "\nOR ",
+        sql_between( 'date', [ map { @{ $_ }{qw/ min max /} } @{ $ranges } ] ) ;
+  }
+
+  $sql = join "\nAND ", grep { $_ } sql_in( 'projectid', $projs ), $sql ;
+
+  $sql or
+    die sprintf <<"NO_SQL", Dumper( $projs, $ranges );
+Query statement could not be generated for projects & semesters: (%s).
+NO_SQL
+
+  return <<"_SQL_";
+    SELECT projectid, timespent, date, confirmed
+    FROM omptimeacct
+    WHERE $sql
+    ORDER BY projectid, date
+_SQL_
+}
+
+sub sql_in {
+
+  my ( $col, $in , $no_quote ) = @_;
+
+  return unless $col && scalar @{ $in };
+
+  return
+    "$col IN ( "
+    . join( ' , ', map {  ( $no_quote ? $_ : "'$_'" ) } @{ $in } )
+    . ' )'
+    ;
+}
+
+sub sql_between {
+
+  my ( $col, $in , $no_quote ) = @_;
+
+  return unless $in && ref $in;
+
+  my $size = scalar @{ $in };
+  return unless $size && 0 == $size % 2;
+
+  my @out;
+  for ( my $i = 0; $i < $size -1 ; $i += 2 ) {
+
+    push @out,
+      "$col BETWEEN "
+      . join ' AND ',
+          map {  $no_quote ? $_ : "'$_'" } @{ $in }[ $i, $i + 1 ]
+          ;
+  }
+
+  return @out;
+}
+
+#  Process options.
+sub process_options {
+
+  my ( %opt ) = @_;
+
+  my ( $help, @file, @sem );
+  GetOptions(
+    'h|help|man' => \$help,
+    'file=s'     => \@file,
+    'header|'    => \$opt{'header'},
+    'H|noheader' => sub{ $opt{'header'} = 0 },
+    'semester=s'  => \@sem,
+    'telescope=s' => \$opt{'telescope'},
+  )
+    # As of Pod::Usage 1.30, C<'-sections' => 'OPTIONS', '-verbose' => 99> does
+    # not output only the OPTIONS part.  Also note that in pod, key is listed
+    # '-section' but in code '-sections' is actually used.
+    or pod2usage( '-exitval' => 2, '-verbose' => 1,);
+
+  pod2usage( '-exitval' => 1, '-verbose' => 2 ) if $help;
+
+  # Print header at least once in the beginning by default.
+  $opt{'header'}++ unless defined $opt{'header'};
+
+  if ( $opt{'telescope'} ) {
+
+    for  my $sem ( @sem ) {
+
+      my %range;
+      @range{qw/ min max /} =
+        map { $_->ymd }
+          OMP::General
+          ->semester_boundary( 'tel' => $opt{'telescope'}, 'semester' => $sem )
+          ;
+
+      push @{ $opt{'date-range'} }, \%range;
+    }
+  }
+
+  $opt{'projects'} =
+    [ map { $_ ? uc $_ : () } @ARGV, get_projects( @file ) ] ;
+
+  pod2usage( '-exitval' => 2, '-verbose' => 99, '-sections' => 'SYNOPSIS',
+              '-msg' => 'Neither project ids nor semester with telescope were given.'
+            )
+    if ! ( $opt{'telescope'} || scalar @sem )
+    && ! scalar @{ $opt{'projects'} }
+    ;
+
+  return %opt;
+}
+
+# Returns ...
+#   - project ids
+#
+# ... read from ...
+#   - array reference of file names or from standard input.
 sub get_projects {
 
-  my ( $opt, @proj ) = @_;
+  my ( @file ) = @_;
 
-  my @file = @{ $opt->{'file'} };
+  return unless scalar @file;
 
   push @file, '-'
     if -p *STDIN && ! first { $_ eq '-' } @file;
 
+  my @proj;
   for my $file ( @file ) {
 
     my $fh = ( openhandle $file ? $file : open_file( $file ) ) or next;
@@ -148,7 +251,11 @@ sub get_projects {
   return @proj;
 }
 
-#  Returns the file handle after opening the given file name.
+# Returns ...
+#   - file handle
+#
+# ... given ...
+#   - file name.
 sub open_file {
 
   my ( $file ) = @_;
@@ -168,72 +275,19 @@ sub open_file {
   return $fh;
 }
 
-# Returns the C<OMP::TimeAcctQuery> object given a project id.
-sub make_query {
+sub is_non_empty_aref { return $_[0] && ref $_[0] && scalar @{ $_[0] }; }
 
-  my ( $proj ) = @_;
+# Returns ...
+#   - the first argument given (error string)
+#
+# ... given ...
+#   - a list of strings to be logged.
+sub log_db_error {
 
-  my $root = 'TimeAcctQuery';
-  my $id = 'projectid';
-  return
-    OMP::TimeAcctQuery->new( 'XML' => wrap( $root => wrap( $id => $proj ) ) );
-}
+  my $err = shift;
+  OMP::General->log_message( join( ': ', $0, $err, @_ ), OMP__LOG_ERROR );
 
-# Returns a string of given a list of values wrapped in the given tag.
-sub wrap {
-
-  my ( $tag, @val ) = @_;
-
-  return qq{<$tag>} . join( ' ' , @val ) . qq{</$tag>};
-}
-
-#  Process options.
-sub process_options {
-
-  my ( %opt ) = @_;
-
-  my ( $help, @file );
-  GetOptions(
-    'h|help|man' => \$help,
-    'file=s'     => \@file,
-    'header+'    => \$opt{'header'},
-    'H|noheader' => sub{ $opt{'header'} = 0 },
-    'divider!'    => \$opt{'divider'},
-    'date=s@'      => $opt{'date'},
-    'start-date=s' => \$opt{'start-date'},
-    'end-date=s'   => \$opt{'end-date'},
-    'tz|timezone'  => \$opt{'timezone'},
-  )
-    # As of Pod::Usage 1.30, C<'-sections' => 'OPTIONS', '-verbose' => 99> does
-    # not output only the OPTIONS part.  Also note that in pod, key is listed
-    # '-section' but in code '-sections' is actually used.
-    or pod2usage( '-exitval' => 2, '-verbose' => 1,);
-
-  pod2usage( '-exitval' => 1, '-verbose' => 2 ) if $help;
-
-  $opt{'file'} = [ @file ] if scalar @file;
-
-  # Print header at least once in the beginning by default.
-  $opt{'header'}++ unless defined $opt{'header'};
-
-  die 'Date options have not been implemented yet.'
-    if scalar
-          map
-          { my $v = $opt{ $_ };
-            ( ref $v ? scalar @{ $v } : $v )
-            ? undef
-              : ()
-          }
-          qw/ date start-date end-date /;
-
-  # If one date point for date range is given, other must be specified.
-  pod2usage( '-msg' => 'Both dates must be specified for a date range.',
-              '-exitval' => 2, '-verbose' => 1,
-            )
-    if $opt{'start-date'} && ! $opt{'end-date'}
-    or ! $opt{'start-date'} && $opt{'end-date'}  ;
-
-  return %opt;
+  return $err;
 }
 
 =pod
@@ -244,13 +298,34 @@ omp-time-acct.pl - Show time spent on a project as CSV
 
 =head1 SYNOPSIS
 
-omp-time-acct.pl I<project-id> [ I<project-id> ... ]
+  Project ids provided as arguments (lists all the time spent on given
+  projects) ...
 
-omp-time-acct.pl -f I<file-containing-project-ids>
+    omp-time-acct.pl project-id [ project-id ... ]
 
-omp-time-acct.pl < I<file-containing-project-ids>
 
-echo I<project-id> | omp-time-acct.pl
+  ... or, in a file ...
+
+    omp-time-acct.pl -f project-id-file
+
+
+  ... or, on standard input ...
+
+    echo project-id | omp-time-acct.pl
+
+
+  Telescope and semester provided (lists all the projects in semester
+  08A, where date range of the semester is as appropriate for JCMT)
+  ...
+
+    omp-time-acct.pl -semester 08A -telescope jcmt
+
+
+  Project id, semester and telescope are provided (lists time spent on
+  given project in the semester 08A as appropriate for UKIRT) ...
+
+    omp-time-acct.pl -semester 08A -telescope ukirt project-id
+
 
 =head1 DESCRIPTION
 
@@ -264,6 +339,13 @@ output format is ...
 Project ids can be specified as arguments to the program, given in a
 file, or provided on standard input.
 
+If semester is provided and also specify the telescope.
+
+Semester can be given with or without a project id.  If a project id
+is not given, then the listing would have all the projects in a given
+semester for any telescope.  Otherwise, listing would have accounting
+for given projects in a given semester.
+
 =head2 OPTIONS
 
 =over 2
@@ -271,47 +353,6 @@ file, or provided on standard input.
 =item B<-help> | B<-man>
 
 Show the full help message.
-
-=item B<-date> I<yyyy.mm.dd.hh.mm.ss>
-
-Specify a date in UTC time zone, not used for date range. (See
-I<timezone> option to other time zone.)
-
-It can be used mulitple times, with or without date range (see
-I<start-date> and I<end-date> options).
-
-A '/' or a '-' is also ok in place of '.', so is lack of any of the
-three characters mentioned.  A date can be specified in one of
-the following formats ...
-
-=over 2
-
-=item *
-
-yyyy.mm.dd.hh.mm.ss
-
-=item *
-
-yyyy.mm.dd.hh.mm
-
-=item *
-
-yyyy.mm.dd
-
-=back
-
-=item B<-divider>
-
-Print a blank line after output of each project id.  Default is not to
-print the blank line.
-
-=item B<-end-date> I<yyyy.mm.dd.hh.mm.ss>
-
-Specify end date, inclusive, for the date range.
-
-Both this option and I<start-date> must be given, when  providing a
-date range.  The date range can be specified in addition to I<date>
-option.  See I<date> option description on acceptable date formats.
 
 =item B<-file> file-containing-project-ids
 
@@ -322,25 +363,30 @@ File name of I<-> specifies to read from standard input.
 
 =item B<-header>
 
-If specified once, print the header only in the beginning (default).
-
-If specified more than once, header is printed before output for each
-project id.
+Specify if to print a header (default).
 
 =item B<-noheader> | B<-H>
 
 Turn off header printing.
 
-=item B<-start-date> I<yyyy.mm.dd.hh.mm.ss>
+=item B<-semester> <semester_1, [semester_2, ...]>
 
-Specify starting date, inclusive, for the date range. See I<end-date>
-option for other details.
+Specify a semester.
 
-=item B<-timezone> | B<-tz> I<time zone>
+It can be used multiple times to specify multiple semesters.
 
-Specify a time zone for all the dates given, default is UTC.
+The I<telescope> option would also needed to be specified if a
+semester is given.
 
-Dates will be shifted to UTC time zone (from the given time zone).
+=item B<-telescope> | B<-tel> <jcmt | ukirt>
+
+Specify a telescope to be used with all the given semesters.  It is
+I<required> if a I<-semester> option was specified.
+
+I<This is used only to find a date range from given semester as
+applicable for the given telescope.>  In other words, this option is
+not used to filter out projects related to one telescope (use project
+ids).
 
 =back
 
