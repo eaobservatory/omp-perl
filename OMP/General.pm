1d662772f932d395ff10e737bd9f137e93ec82f7
Tim Jenness <t.jenness@jach.hawaii.edu>
2008-08-07 20:36:51 +0000

    remove debug
    svn path=/trunk/omp/perl/; revision=15903

--- OMP/General.pm
+++ OMP/General.pm
@@ -543,10 +543,8 @@ sub _process_freeut_range {
       my $sundef = Astro::Coords::SUN_RISE_SET();
       if ($mode =~ /rise$/) {
         $out = $Sun->rise_time( event => 1, horizon => $sundef );
-        print "Sunrise $out\n";
       } elsif ($mode =~ /set$/) {
         $out = $Sun->set_time( event => -1, horizon => $sundef);
-        print "Sunset $out\n";
       } else {
         throw OMP::Error::FatalError("Odd programming error");
       }
