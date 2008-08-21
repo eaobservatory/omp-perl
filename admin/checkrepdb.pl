a4904b790ad65841a48d1ef31520074b39737bc6
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2008-08-21 00:55:44 +0000

    - Refactor large loop body into subs.
    - Add an option to see the progress for the program (a) sleeps for 20s, (b)
      checking for truncated science programs takes noticeable time.
    
    - Update pod.
    
    svn path=/trunk/omp/perl/; revision=15978

--- admin/checkrepdb.pl
+++ admin/checkrepdb.pl
@@ -15,7 +15,7 @@ use OMP::KeyDB;
 
 my $msg;
 my $trunc;
-my $missing;
+my $missing_prog;
 my $row_count;
 my $missing_msb;
 my $critical;
@@ -26,7 +26,7 @@ my $fault = 0;
 my $primary_db = "SYB_JAC";
 my $secondary_db = "SYB_JAC2";
 
-my ( @to_addr, @cc_addr, $debug);
+my ( @to_addr, @cc_addr, $debug, $progress );
 
 {
   my $help;
@@ -36,7 +36,8 @@ my ( @to_addr, @cc_addr, $debug);
     's|sec|secondary=s' => \$secondary_db,
     'to=s' => \@to_addr,
     'cc=s' => \@cc_addr,
-    'debug' => \$debug
+    'debug' => \$debug,
+    'progress' => \$progress
   ) || die pod2usage( '-exitval' => 2, '-verbose' => 1 );
 
   pod2usage( '-exitval' => 1, '-verbose' => 2 ) if $help;
@@ -54,6 +55,9 @@ my $secondary_db_down;
 # Do insert on primary DB
 $ENV{OMP_DBSERVER} = $primary_db;
 try {
+
+  progress( "Checking if $primary_db is up" );
+
   $primary_kdb = new OMP::KeyDB( DB => new OMP::DBbackend );
 } catch OMP::Error with {
   my $E = shift;
@@ -63,13 +67,20 @@ try {
 
 $msg .= make_server_status( $primary_db, $primary_db_down );
 
+progress( "Generating a key in database on $primary_db" );
+
 my $key = !!$primary_kdb && $primary_kdb->genKey();
+
+progress( "Sleeping for 20 seconds" );
 
 sleep 20;
 
 # Look for insert on replicate DB
 $ENV{OMP_DBSERVER} = $secondary_db;
 try {
+
+  progress( "Checking if $secondary_db is up" );
+
   $secondary_kdb = new OMP::KeyDB( DB => new OMP::DBbackend(1) )
 } catch OMP::Error with {
   $secondary_db_down = 1;
@@ -77,6 +88,8 @@ try {
 };
 
 $msg .= make_server_status( $secondary_db, $secondary_db_down );
+
+progress( "Verifying key in database on $secondary_db" );
 
 my $verify = !!$secondary_kdb && $secondary_kdb->verifyKey($key);
 
@@ -97,145 +110,17 @@ Replication server ($primary_db -> $secondary_db) is not replicating!
 REP_SERVER
   }
 
-  # Check for truncated science programs
-  my $sql = "SELECT projectid FROM ompsciprog WHERE sciprog not like \"%</SpProg>%\"";
+  my @msg;
+  ( $msg[0], $trunc ) = check_truncated_sciprog();
 
-  my %results;
-  for my $dbserver ( $primary_db, $secondary_db ) {
-    $ENV{OMP_DBSERVER} = $dbserver;
-    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
+  ( $msg[1], $missing_prog ) = check_missing_sciprog();
 
-    my $ref = $db->_db_retrieve_data_ashash( $sql );
+  ( $msg[2], $row_count ) = compare_row_count();
 
-    if ($ref->[0]) {
-      map {push @{$results{$dbserver}}, $_->{projectid}} @$ref;
-    }
-  }
+  ( $msg[3], $missing_msb ) = check_missing_msb();
 
-  if (%results) {
-    $trunc = 1;
-    $fault++;
-    $msg .= "Truncated science programs found!\n";
-    for my $dbserver (keys %results) {
-      $msg .= "$dbserver:\n";
-      map {$msg .= "\t$_\n"} @{$results{$dbserver}};
-      $msg .= "\n";
-    }
-  } else {
-    $msg .= "No truncated science programs found. [OK]\n\n";
-  }
+  $msg .= join '', grep { $_ } @msg;
 
-  # Check for missing science programs
-  my $missing_sql = "SELECT DISTINCT F.projectid\n".
-    "FROM ompfeedback F, ompsciprog S\n".
-      "WHERE F.projectid = S.projectid\n".
-        "AND F.msgtype = 70\n".
-          "AND S.sciprog LIKE NULL";
-
-  my %missing_results;
-  for my $dbserver ( $primary_db, $secondary_db ) {
-    $ENV{OMP_DBSERVER} = $dbserver;
-    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
-
-    my $ref = $db->_db_retrieve_data_ashash( $sql );
-
-        if ($ref->[0]) {
-      map {push @{$results{$dbserver}}, $_->{projectid}} @$ref;
-    }
-  }
-
-  if (%missing_results) {
-    $missing = 1;
-    $fault++;
-    $msg .= "Missing programs detected!\n";
-    for my $dbserver (keys %results) {
-      $msg .= "$dbserver:\n";
-      map {$msg .= "\t$_\n"} @{$results{$dbserver}};
-      $msg .= "\n";
-    }
-  } else {
-    $msg .= "All science programs are present. [OK]\n\n";
-  }
-
-  # Compare row counts for disparity
-  my %row_count_results;
-  for my $dbserver ( $primary_db, $secondary_db ) {
-    $ENV{OMP_DBSERVER} = $dbserver;
-    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
-    for my $table (qw/ompfault ompfaultassoc ompfaultbody ompfeedback
-                      ompmsb ompmsbdone ompobs ompobslog ompproj
-                      ompprojqueue ompprojuser ompshiftlog ompsciprog
-                      omptimeacct ompuser ompkey/) {
-      my $sql = "SELECT COUNT(*) AS \'count\' FROM $table";
-
-      my $ref = $db->_db_retrieve_data_ashash( $sql );
-      $row_count_results{$dbserver}{$table} = $ref->[0]->{count};
-    }
-  }
-
-  my @row_count_disparity;
-  for my $table (keys %{$row_count_results{ $primary_db }}) {
-    if ($row_count_results{ $primary_db }{$table} ne $row_count_results{ $secondary_db }{$table}) {
-      push(@row_count_disparity, $table)
-        unless $table eq 'ompkey';
-    }
-  }
-
-  if ($row_count_disparity[0]) {
-    $row_count = 1;
-    $fault++;
-    $msg .= <<"DISPARITY";
-Disparity in row counts detected between $primary_db & $secondary_db!
-The following tables are affected:
-DISPARITY
-
-    $msg .=
-      make_diff_status( $row_count_results{ $primary_db },
-                        $row_count_results{ $secondary_db },
-                        @row_count_disparity
-                      );
-    $msg .= "\n";
-  } else {
-    $msg .= "Row counts are equal across both databases. [OK]\n\n";
-  }
-
-  # Make sure MSBs are present if there's a science program
-  for my $dbserver ( $primary_db, $secondary_db ) {
-    $ENV{OMP_DBSERVER} = $dbserver;
-    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
-    my $sql = "SELECT projectid FROM ompsciprog\n".
-      "WHERE projectid NOT IN (SELECT DISTINCT projectid FROM ompmsb)";
-
-    my $ref = $db->_db_retrieve_data_ashash( $sql );
-
-    my @really_missing;
-    if ($ref->[0]) {
-      for my $row (@$ref) {
-        my $projectid = $row->{projectid};
-        my $sql = "SELECT projectid FROM ompsciprog\n".
-          "WHERE projectid = '$projectid' AND sciprog NOT LIKE '%<SpObs%'";
-
-        my $really_missing_ref = $db->_db_retrieve_data_ashash( $sql );
-        push @really_missing, $projectid
-          unless ($really_missing_ref->[0]);
-      }
-    }
-
-    if ($really_missing[0]) {
-      $missing_msb = 1;
-      $fault++;
-
-      $msg .= "Missing MSBs detected on ${dbserver}!\n";
-      $msg .= "The following projects are affected:\n";
-      for my $projectid (@really_missing) {
-        $msg .= "\t$projectid\n";
-      }
-      $msg .= "\n";
-    }
-  }
-  if (! $missing_msb) {
-    $msg .= "MSBs are present for each science program. [OK]\n\n";
-  }
 }
 
 my $subject;
@@ -245,7 +130,7 @@ if ($critical) {
   $subject = 'MULTIPLE FAULTS!';
 } elsif ($trunc) {
   $subject = 'TRUNCATED PROGRAMS FOUND!';
-} elsif ($missing) {
+} elsif ($missing_prog) {
   $subject = 'MISSING PROGRAMS DETECTED!';
 } elsif ($row_count) {
   $subject = 'ROW COUNT DISPARITY DETECTED!';
@@ -271,6 +156,199 @@ else {
 }
 
 exit;
+
+sub check_missing_msb {
+
+  # Make sure MSBs are present if there's a science program
+  progress( "Checking for MSB in science program" );
+
+  my ( $msg ) = ( '' ) ;
+  for my $dbserver ( $primary_db, $secondary_db ) {
+
+    $ENV{OMP_DBSERVER} = $dbserver;
+    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
+
+    my $sql = "SELECT projectid FROM ompsciprog\n".
+      "WHERE projectid NOT IN (SELECT DISTINCT projectid FROM ompmsb)";
+
+    my $ref = $db->_db_retrieve_data_ashash( $sql );
+
+    my @really_missing;
+    if ($ref->[0]) {
+
+      for my $row (@$ref) {
+
+        my $projectid = $row->{projectid};
+        my $sql = "SELECT projectid FROM ompsciprog\n".
+          "WHERE projectid = '$projectid' AND sciprog NOT LIKE '%<SpObs%'";
+
+        my $really_missing_ref = $db->_db_retrieve_data_ashash( $sql );
+        push @really_missing, $projectid
+          unless $really_missing_ref->[0];
+      }
+    }
+
+    if ($really_missing[0]) {
+
+      $missing_msb = 1;
+      $fault++;
+
+      $msg .= "Missing MSBs detected on ${dbserver}!\n"
+            . "The following projects are affected:\n"
+            . join "\t", map { "$_\n" } @really_missing
+            ;
+
+      $msg .= "\n";
+    }
+  }
+
+  if (! $missing_msb) {
+    $msg = "MSBs are present for each science program. [OK]\n\n";
+  }
+
+  return ( $msg, $missing_msb );
+}
+
+sub compare_row_count {
+
+  # Compare row counts for disparity
+  progress( "Comparing row count" );
+
+  my %row_count_results;
+  for my $dbserver ( $primary_db, $secondary_db ) {
+
+    $ENV{OMP_DBSERVER} = $dbserver;
+    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
+
+    for my $table (qw/ompfault ompfaultassoc ompfaultbody ompfeedback
+                      ompmsb ompmsbdone ompobs ompobslog ompproj
+                      ompprojqueue ompprojuser ompshiftlog ompsciprog
+                      omptimeacct ompuser ompkey/) {
+
+      my $sql = "SELECT COUNT(*) AS \'count\' FROM $table";
+
+      my $ref = $db->_db_retrieve_data_ashash( $sql );
+      $row_count_results{$dbserver}{$table} = $ref->[0]->{count};
+    }
+  }
+
+  my @row_count_disparity;
+  for my $table (keys %{$row_count_results{ $primary_db }}) {
+    if ($row_count_results{ $primary_db }{$table} ne $row_count_results{ $secondary_db }{$table}) {
+      push(@row_count_disparity, $table)
+        unless $table eq 'ompkey';
+    }
+  }
+
+  my ( $msg, $fault );
+  if ($row_count_disparity[0]) {
+
+    $row_count = 1;
+    $fault++;
+
+    $msg = <<"DISPARITY";
+Disparity in row counts detected between $primary_db & $secondary_db!
+The following tables are affected:
+DISPARITY
+
+    $msg .=
+      make_diff_status( $row_count_results{ $primary_db },
+                        $row_count_results{ $secondary_db },
+                        @row_count_disparity
+                      );
+    $msg .= "\n";
+  } else {
+
+    $msg = "Row counts are equal across both databases. [OK]\n\n";
+  }
+
+  return ( $msg, $row_count );
+}
+
+sub check_missing_sciprog {
+
+  # Check for missing science programs
+  progress( "Checking for missing science program" );
+
+  my $sql = "SELECT DISTINCT F.projectid\n".
+    "FROM ompfeedback F, ompsciprog S\n".
+      "WHERE F.projectid = S.projectid\n".
+        "AND F.msgtype = 70\n".
+          "AND S.sciprog LIKE NULL";
+
+  my %results;
+  for my $dbserver ( $primary_db, $secondary_db ) {
+    $ENV{OMP_DBSERVER} = $dbserver;
+    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
+
+    my $ref = $db->_db_retrieve_data_ashash( $sql );
+
+        if ($ref->[0]) {
+      map {push @{$results{$dbserver}}, $_->{projectid}} @$ref;
+    }
+  }
+
+  my $msg;
+  if (%results) {
+
+    $missing_prog = 1;
+    $fault++;
+    $msg = "Missing programs detected!\n";
+
+    for my $dbserver (keys %results) {
+      $msg .= "$dbserver:\n";
+      map {$msg .= "\t$_\n"} @{$results{$dbserver}};
+      $msg .= "\n";
+    }
+  } else {
+
+    $msg = "All science programs are present. [OK]\n\n";
+  }
+
+  return ( $msg, $missing_prog );
+}
+
+sub check_truncated_sciprog {
+
+  # Check for truncated science programs
+  progress( "Checking for truncated science program" );
+
+  my $sql = "SELECT projectid FROM ompsciprog WHERE sciprog not like \"%</SpProg>%\"";
+
+  my %results;
+  for my $dbserver ( $primary_db, $secondary_db ) {
+
+    $ENV{OMP_DBSERVER} = $dbserver;
+    my $db = new OMP::BaseDB( DB => new OMP::DBbackend(1) );
+
+    my $ref = $db->_db_retrieve_data_ashash( $sql );
+
+    if ($ref->[0]) {
+      map {push @{$results{$dbserver}}, $_->{projectid}} @$ref;
+    }
+  }
+
+  my ( $msg, $trunc );
+  if (%results) {
+
+    $trunc = 1;
+    $fault++;
+
+    $msg = "Truncated science programs found!\n";
+
+    for my $dbserver (keys %results) {
+
+      $msg .= "$dbserver:\n";
+      map {$msg .= "\t$_\n"} @{$results{$dbserver}};
+      $msg .= "\n";
+    }
+  } else {
+
+    $msg = "No truncated science programs found. [OK]\n\n";
+  }
+
+  return ( $msg, $trunc );
+}
 
 sub send_mail {
 
@@ -315,6 +393,12 @@ sub make_diff_status {
   return $text;
 }
 
+sub progress {
+
+  $progress and warn @_ , ( $_[-1] =~ m/\n$/ ? () : "\n" );
+  return;
+}
+
 =pod
 
 =head1 NAME
@@ -326,6 +410,7 @@ checkrepdb.pl - Check database replication
   checkrepdb.pl -help
 
   checkrepdb.pl \
+    [ -progress ] \
     [-p <primary db server>] [-s <secondary db server>] \
     [ -to root@example.org [, -to tech@example.org ] ] \
     [ -cc support@example.net [, -cc sales@example.com ] ]
@@ -354,11 +439,15 @@ It can be specified multiple times to send email to more than one address.
 
 =item B<-p> <db server> | B<-primary> <db server>
 
-Specify primary database server (source); default is "SYB_OMP1".
+Specify primary database server (source); default is "SYB_JAC".
 
 =item B<-s> <db server> | B<-secondary> <db server>
 
-Specify secondary database server (replicate); default is "SYB_OMP2".
+Specify secondary database server (replicate); default is "SYB_JAC2".
+
+=item B<-progress>
+
+Prints what is going to be done next on standard error.
 
 =back
 
