44e57ff776ca1a04cd21e3d01b976d0f3219b91b
Anubhav Agarwal <a.agarwal@jach.hawaii.edu>
2008-09-26 04:22:39 +0000

    - Change servers from omp[12] to omp[34].
    svn path=/trunk/omp/perl/; revision=16064

--- admin/ompbak.pl
+++ admin/ompbak.pl
@@ -5,5 +5,5 @@ my $tarfile = 'ompbak_'. $date->strftime("%Y%m%d") .'.tar.gz';
 
 chdir "/export/data/jcmtarch/ompbak";
 
-my $output = qx!/bin/tar -zcvf $tarfile omp1 omp2!
+my $output = qx!/bin/tar -zcvf $tarfile omp3 omp4!
   or die "Error creating tar archive: $!\n";
