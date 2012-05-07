dcfb5abac1d5fbd47c542ef9dcaf366a6babee42
Graham Bell <g.bell@jach.hawaii.edu>
2012-05-07 13:16:22 -1000

    Moving regions code from listtargs to a module. (cherry picked from commit 9e8175dbb1bfac947ea1db86b0d0ddb5aa42341c)

---
+++ OMP/SpRegion.pm
@@ -0,0 +1,438 @@
+package OMP::SpRegion;
+
+=head1 NAME
+
+OMP::SpRegion - Create AST regions for Science Programs
+
+=head1 SYNOPSIS
+
+  my $sp = OMP::SpServer->fetchProgram(...);
+
+  my $region = new OMP::SpRegion($sp);
+
+  $region->write_stcs(type => 'all');
+  $region->write_ast(type => 'all');
+
+  # Prepare PGPLOT device.
+  $region->plot_pgplot(type => 'all', method => 'separate');
+
+=head1 DESCRIPTION
+
+This class can be used to create AST regions for all of the
+observations in a Science Programme.
+
+=cut
+
+use strict;
+use warnings;
+
+use Starlink::AST;
+use Astro::Coords;
+use Astro::Coords::Offset;
+use Astro::PAL;
+
+# Calculate bounds for Polygons and CmpRegions manually?
+use constant WORKAROUND_BOUNDS => 1;
+
+=head1 DATA
+
+=over 4
+
+=item B<%fov_inst>
+
+Specifies the field of view for each instrument, to be used as the
+minimum field size.
+
+=cut
+
+# Data copied from JCMT::Survey::Coverage::RADIUS because
+# it is a private variable in that package and adding SCUBA
+# to allow older projects to be plotted.
+our %fov_inst = (
+              HARP      => 60,
+              SCUBA     => 2.3 * 60,
+              'SCUBA-2' => 5 * 60,
+            );
+
+=back
+
+=head1 METHODS
+
+=head2 Constructor
+
+=over 4
+
+=item B<new>
+
+Reads the specified Science Programme and constructs an object containing
+AST regions for the objects within.
+
+  my $region = new OMP::SpRegion($sp);
+
+=cut
+
+sub new {
+  my $class = shift;
+  my $sp = shift;
+  my %opt = @_;
+
+
+  my $self = {
+    'cmp' => {},
+    separate => {all => [], 'new' => [], progress => [], complete => []},
+    lbnd => [],
+    ubnd => [],
+  };
+
+  bless $self, $class;
+  my %uniq = (all => {}, 'new' => {}, progress => {}, complete => {});
+  my $skyframe = new Starlink::AST::SkyFrame('SYSTEM=FK5');
+
+  foreach my $msb ($sp->msb) {
+    my $remaining = $msb->remaining();
+    my $observed  = $msb->observed();
+
+    foreach my $obs ($msb->unroll_obs()) {
+      # Retrieve Astro::Coords object
+      my $coords = $obs->{'coords'};
+      next if $coords->type() eq 'CAL';
+      #use Data::Dumper; print Dumper($obs);
+
+      if (exists $obs->{'OFFSET_DX'} or exists $obs->{'OFFSET_DY'}) {
+        my $offset = new Astro::Coords::Offset(
+          $obs->{'OFFSET_DX'} || 0,
+          $obs->{'OFFSET_DY'} || 0,
+          posang   => $obs->{'OFFSET_PA'},
+          'system'   => $obs->{'OFFSET_SYSTEM'} || 'J2000',
+          projection => 'TAN');
+
+        $coords = $coords->apply_offset($offset);
+      }
+
+      my $fov = $fov_inst{$obs->{'instrument'}} || 1;
+      my $height = $obs->{'MAP_HEIGHT'}    || $fov;
+      my $width  = $obs->{'MAP_WIDTH'}     || $fov;
+      $height    = $fov if $height < $fov;
+      $width     = $fov if $width < $fov;
+      my $radius = Astro::PAL::DAS2R
+                 * (($height > $width) ? $height : $width) / 2;
+
+      # Create a key so that we can check by uniqueness.
+      my $id = sprintf('%.6f', $coords->ra2000()->radians())
+       . ' ' . sprintf('%.6f', $coords->dec2000()->radians())
+       . ' ' . $radius. "\n";
+
+      my $region = undef;
+
+      unless (lc($obs->{'scanPattern'}) eq 'boustrophedon'
+           or lc($obs->{'scanPattern'}) eq 'raster') {
+        $region = Starlink::AST::Circle->new(
+                             $skyframe, 1, [$coords->ra2000()->radians(),
+                                        $coords->dec2000()->radians()],
+                             [$radius], undef, '');
+
+        WORKAROUND_BOUNDS && $self->_add_bounds($region);
+      }
+      else {
+        my @corner = ();
+        foreach my $cf ([1, 1], [-1, 1], [-1, -1], [1, -1]) {
+          my $offset = new Astro::Coords::Offset(
+                             $cf->[0] * $width / 2, $cf->[1] * $height / 2,
+                             posang     => $obs->{'MAP_PA'},
+                             'system'   => 'J2000',
+                             projection => 'TAN');
+          push @corner, $coords->apply_offset($offset);
+
+          WORKAROUND_BOUNDS && $self->_add_bounds(
+            $corner[-1]->ra2000()->radians(),
+            $corner[-1]->dec2000()->radians());
+        }
+
+        $region = new Starlink::AST::Polygon($skyframe,
+                                    [map {$_->ra2000()->radians()} @corner],
+                                    [map {$_->dec2000()->radians()} @corner],
+                                    undef, '');
+      }
+
+      die 'OMP::SpRegion: failed to create region' unless defined $region;
+
+      $self->_add_region('all', $region) unless exists $uniq{'all'}->{$id};
+      $uniq{'all'}->{$id} = 1;
+
+      # Rearranged the if statements into this order to handle the
+      # case where the 'observed' field is empty and shows 0 for everything:
+      # now in-progress observations appear new rather than complete.
+      if (! $remaining > 0 and ! exists $uniq{'complete'}->{$id}) {
+        $self->_add_region('complete', $region);
+        $uniq{'complete'}->{$id} = 1;
+      }
+      elsif (! $observed and ! exists $uniq{'new'}->{$id}) {
+        $self->_add_region('new', $region);
+        $uniq{'new'}->{$id} = 1;
+      }
+      elsif (! exists $uniq{'progress'}->{$id}) {
+        $self->_add_region('progress', $region);
+        $uniq{'progress'}->{$id} = 1;
+      }
+    }
+  }
+
+  return $self;
+}
+
+=back
+
+=head2 General Methods
+
+=over 4
+
+=item B<write_ast>
+
+Writes an AST CmpRegion file to standard output.  An optional C<type> parameter
+can specify the type of MSBs to be included:
+
+=over 4
+
+=item *
+
+all
+
+=item *
+
+new: MSBs which have not been observed
+
+=item *
+
+progress: MSBs which have been observed but are not complete
+
+=item *
+
+complete: MSBs with no repeats remaining
+
+=back
+
+=cut
+
+sub write_ast {
+  my $self = shift;
+  my %opt = @_;
+  my $type = $opt{'type'} || 'all';
+  die 'OMP::SpRegion: type must be one of: ' . join ', ', keys %{$self->{'separate'}}
+    unless exists $self->{'separate'}{$type};
+  my $cmp = $self->{'cmp'}->{$type};
+  $cmp->Show();
+}
+
+=item B<write_stcs>
+
+Writes an STCS description to standard output, taking a C<type> parameter
+as for C<write_ast>.
+
+=cut
+
+sub write_stcs {
+  my $self = shift;
+  my %opt = @_;
+  my $type = $opt{'type'} || 'all';
+  die 'OMP::SpRegion: type must be one of: ' . join ', ', keys %{$self->{'separate'}}
+    unless exists $self->{'separate'}{$type};
+  my $cmp = $self->{'cmp'}->{$type};
+  my $ch = new Starlink::AST::StcsChan(sink => sub {print "$_[0]\n"});
+  $ch->Write($cmp);
+}
+
+
+=item B<plot_pgplot>
+
+Plots the region with PGPLOT.  This takes a C<type> parameter as for C<write_ast>
+and also a C<method> parameter which can be:
+
+=over 4
+
+=item *
+
+separate: draw each region separately
+
+=item *
+
+cmpregion: plot the whole AST cmpregion
+
+=back
+
+The calling function should already have prepared the PGPLOT device.
+
+  $region->plot_pgplot(type => 'all', method => 'separate');
+
+=cut
+
+sub plot_pgplot {
+  my $self = shift;
+  my %opt = @_;
+  my $type = $opt{'type'} || 'all';
+  die 'OMP::SpRegion: type must be one of: ' . join ', ', keys %{$self->{'separate'}}
+  unless exists $self->{'separate'}{$type};
+
+  my $wcs = $self->_make_wcs();
+
+  my $plot = new Starlink::AST::Plot($wcs, [0.0, 0.0, 1.0, 1.0],
+                                     [0.5, 0.5, 1000.5, 1000.5],
+                 'Grid=1,tickall=0,border=1,tol=0.001'
+                 . ',colour(border)=4,colour(grid)=3,colour(ticks)=3'
+                 . ',colour(numlab)=5,colour(axes)=3');
+  $plot->pgplot();
+  $plot->Grid();
+  $plot->Set('System=FK5');
+
+  my $fitswcsb = $plot->Get('Base');
+  my $fitswcsc = $plot->Get('Current');
+
+  my %colour = (
+    'new' => 1,     # white
+    progress => 2,  # red
+    complete => 4); # blue
+
+  foreach my $colour (keys %colour) {
+    next unless exists $self->{'cmp'}->{$colour};
+    next unless $type eq 'all' or $type eq $colour;
+
+    if ((defined $opt{'method'}) && ($opt{'method'} eq 'cmpregion')) {
+      my $cmp = $self->{'cmp'}->{$colour};
+
+      my $fs = $plot->Convert($cmp, '');
+      $plot->Set('Base='.$fitswcsb);
+      my $map = $fs->GetMapping(Starlink::AST::AST__BASE(),
+                                Starlink::AST::AST__CURRENT());
+      $plot->AddFrame(Starlink::AST::AST__CURRENT(), $map, $cmp);
+      $plot->Set('colour(border)='. $colour{$colour});
+      $plot->Border();
+
+      my $current = $plot->Get('Current');
+      $plot->RemoveFrame($current);
+      $plot->Set('Current='.$fitswcsc);
+    }
+    else {
+      foreach my $cmp (@{$self->{'separate'}->{$colour}}) {
+        my $fs = $plot->Convert($cmp, '');
+        $plot->Set('Base='.$fitswcsb);
+        my $map = $fs->GetMapping(Starlink::AST::AST__BASE(),
+                                  Starlink::AST::AST__CURRENT());
+        $plot->AddFrame(Starlink::AST::AST__CURRENT(), $map, $cmp);
+        $plot->Set('colour(border)='. $colour{$colour});
+        $plot->Border();
+
+        my $current = $plot->Get('Current');
+        $plot->RemoveFrame($current);
+        $plot->Set('Current='.$fitswcsc);
+      }
+    }
+  }
+}
+
+=back
+
+=head2 Internal Methods
+
+=over 4
+
+=item B<_add_region>
+
+Adds a region to the object.  It is stored both as a separate region
+and added to the CmpRegion for the given type.
+
+  $self->_add_region($type, $region);
+
+=cut
+
+sub _add_region {
+  my $self = shift;
+  my ($name, $region) = @_;
+
+  push @{$self->{'separate'}->{$name}}, $region;
+
+  unless (exists $self->{'cmp'}->{$name}) {
+    $self->{'cmp'}->{$name} = $region;
+  }
+  else {
+    $self->{'cmp'}->{$name}
+      = $self->{'cmp'}->{$name}->CmpRegion($region,
+          Starlink::AST::Region::AST__OR(), '');
+  }
+}
+
+=item B<_add_bounds>
+
+If the bounds are being handled 'manually', extend them to include the
+given region or specific point.
+
+  $self->_add_bounds($region);
+  $self->_add_bounds($ra2000radians, $dec2000radians);
+
+=cut
+
+sub _add_bounds {
+  my $self = shift;
+  my $region = shift;
+
+  WORKAROUND_BOUNDS || die 'Calling OMP::SpRegion::_add_bounds unnecessarily';
+
+  my (@l, @u);
+
+  if (ref $region) {
+    $region->GetRegionBounds(\@l, \@u);
+  }
+  else {
+    @l = ($region, shift);
+    @u = @l;
+  }
+
+  foreach (0, 1) {
+    $self->{'lbnd'}->[$_] = $l[$_] if (! defined $self->{'lbnd'}->[$_]
+                                       || $l[$_] < $self->{'lbnd'}->[$_]);
+    $self->{'ubnd'}->[$_] = $u[$_] if (! defined $self->{'ubnd'}->[$_]
+                                       || $u[$_] > $self->{'ubnd'}->[$_]);
+  }
+}
+
+=item B<_make_wcs>
+
+Creates a FITS header specifying a region to encompass the bounds of
+all the regions in the Science Program, and uses this to return
+a suitable AST object.
+
+=cut
+
+sub _make_wcs {
+  my $self = shift;
+
+  my @lbnd = @{$self->{'lbnd'}};
+  my @ubnd = @{$self->{'ubnd'}};
+
+  WORKAROUND_BOUNDS || $self->{'cmp'}->{'all'}->GetRegionBounds(\@lbnd, \@ubnd);
+
+  my $fchan = new Starlink::AST::FitsChan();
+  foreach (
+        'NAXIS1  = 1000',
+        'NAXIS2  = 1000 ',
+        'CRPIX1  = 500 ',
+        'CRPIX2  = 500',
+        'CRVAL1  = ' . Astro::PAL::DR2D * ($lbnd[0] + $ubnd[0]) / 2,
+        'CRVAL2  = ' . Astro::PAL::DR2D * ($lbnd[1] + $ubnd[1]) / 2,
+        'CTYPE1  = \'RA---MER\'',
+        'CTYPE2  = \'DEC--MER\'',
+        'RADESYS = \'FK5\'',
+        'CD1_1   = ' . Astro::PAL::DR2D * ($lbnd[0] - $ubnd[0]) / 1000,
+        'CD2_2   = ' . Astro::PAL::DR2D * ($ubnd[1] - $lbnd[1]) / 1000,
+    ) {$fchan->PutFits($_, 0);}
+  $fchan->Clear('Card');
+  my $wcs = $fchan->Read();
+  return $wcs;
+}
+
+
+1;
+
+__END__
+
+=back
+
+=cut
