1ec4d078164af5b144bbb0acd2323f4646ae3417
Graham Bell <g.bell@jach.hawaii.edu>
2012-05-07 16:39:37 -1000

    Adding science program region plot/download to web
    (Code not yet tested, need to check out to omp-dev.)

---
+++ cgi/spregion.pl
@@ -0,0 +1,30 @@
+#!/local/perl/bin/perl -XT
+use strict;
+
+# Standard initialisation (not much shorter than the previous
+# code but no longer has the module path hard-coded)
+BEGIN {
+  my $retval = do "./omp-cgi-init.pl";
+  unless ($retval) {
+    warn "couldn't parse omp-cgi-init.pl: $@" if $@;
+    warn "couldn't do omp-cgi-init.pl: $!"    unless defined $retval;
+    warn "couldn't run omp-cgi-init.pl"       unless $retval;
+    exit;
+  }
+}
+
+use OMP::CGIPage;
+use OMP::CGIPage::SpRegion;
+
+my $arg = shift @ARGV;
+
+my $q = new CGI;
+my $ompcgi = new OMP::CGIPage( CGI => $q );
+
+my $title = $ompcgi->html_title;
+$ompcgi->html_title("$title: Science Program Regions");
+
+# Use the 'proposals' page template so that we can set an appropriate
+# content type for the region files and plot.
+$ompcgi->write_page_proposals(\&OMP::CGIPage::SpRegion::view_region,
+                              \&OMP::CGIPage::SpRegion::view_region_output);
