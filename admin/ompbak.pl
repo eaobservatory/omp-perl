071fcdb38d055bdedbc51e682ea146316490edce
A Agarwal <a.agarwal@jach.hawaii.edu>
2012-09-28 18:48:55 -1000

    ompbak: Update parent directory path.

--- admin/ompbak.pl
+++ admin/ompbak.pl
@@ -7,7 +7,7 @@ use Time::Piece;
 my $date = localtime;
 my $tarfile = 'ompbak_'. $date->strftime("%Y%m%d") .'.tar.gz';
 
-my $arc_dir = 'export/data/jcmtarch/ompbak';
+my $arc_dir = '/export/data/jcmtdata/jcmtarch/ompbak';
 chdir $arc_dir
   or die "Could not change directory to $arc_dir: $!\n";
 
