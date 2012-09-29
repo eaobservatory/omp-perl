4935f6dfb0f946097b78313ec654fd2d4298ed11
A Agarwal <a.agarwal@jach.hawaii.edu>
2012-09-28 18:11:28 -1000

    ompbak: Check chdir() status; general update, cleansing.

--- admin/ompbak.pl
+++ admin/ompbak.pl
@@ -1,9 +1,18 @@
+#!/local/perl/bin/perl
+
+use warnings; use strict;
+
 use Time::Piece;
 
 my $date = localtime;
 my $tarfile = 'ompbak_'. $date->strftime("%Y%m%d") .'.tar.gz';
 
-chdir "/export/data/jcmtarch/ompbak";
+my $arc_dir = 'export/data/jcmtarch/ompbak';
+chdir $arc_dir
+  or die "Could not change directory to $arc_dir: $!\n";
 
-my $output = qx!/bin/tar -zcvf $tarfile omp3 omp4!
+my $output = qx!/bin/tar -zcvf $tarfile omp3 omp4 2>&1!
   or die "Error creating tar archive: $!\n";
+
+print $output;
+
