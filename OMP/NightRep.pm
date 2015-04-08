85841e55cc2d195d3d6269ac299f71b4b9789ed0
A Agarwal <a.agarwal@jach.hawaii.edu>
2015-04-08 08:17:30 -1000

    NR: Whitespace change.

--- OMP/NightRep.pm
+++ OMP/NightRep.pm
@@ -1014,8 +1014,8 @@ Project Time Summary
 
   my $grp = $self->obs;
   $grp->locate_timegaps( OMP::Config->getData("timegap") );
-  my $tmp =  $grp->summary('72col');
-  $str .= defined $tmp ? $tmp || '';
+  my $tmp = $grp->summary('72col');
+  $str   .= defined $tmp ? $tmp || '';
 
   if (wantarray) {
     return split("\n", $str);
