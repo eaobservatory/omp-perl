2d656bfc3fc92c90a8376a59c7551950bc5170a8
A Agarwal <a.agarwal@jach.hawaii.edu>
2016-02-11 07:40:06 -1000

    ol: Remove "-font" option as *::DialogBox doesn't care for it.
    There shouldn't any need to revert previous font change. (Having no observations
    would trigger error handler which would invoke Tk::DialogBox which would then
    die with error due to unknown option given.)

--- client/obslog.pl
+++ client/obslog.pl
@@ -853,7 +853,6 @@ sub rescan {
     new_instrument( "NONE", undef, 1 );
     my $dbox = $MainWindow->DialogBox( -title => "Error",
                                        -buttons => ["OK"],
-                                        -font => $opt{'font-var'},
                                      );
 
     my $label = $dbox->add( 'Label',
@@ -871,7 +870,6 @@ sub rescan {
     new_instrument( "NONE", undef, 1 );
     my $dbox = $MainWindow->DialogBox( -title => "Error",
                                        -buttons => ["OK"],
-                                        -font => $opt{'font-var'},
                                      );
 
     my $label = $dbox->add( 'Label',
@@ -945,7 +943,6 @@ sub dump_to_disk {
   close $fh;
   my $dbox = $MainWindow->DialogBox( -title => "File Saved",
                                      -buttons => ["OK"],
-                                      -font => $opt{'font-var'},
                                    );
   my $label = $dbox->add( 'Label',
                           -text => "Data has been saved in " . $filename )->pack;
@@ -1857,7 +1854,6 @@ sub update_shiftlog_comments {
     require Tk::DialogBox;
     my $dbox = $MainWindow->DialogBox( -title => "Error",
                                        -buttons => ["OK"],
-                                        -font => $opt{'font-var'},
                                      );
     my $label = $dbox->add( 'Label',
                             -text => "Error: " . $Error->{-text},
@@ -1874,10 +1870,8 @@ sub update_shiftlog_comments {
   otherwise {
     my $Error = shift;
     require Tk::DialogBox;
-
     my $dbox = $MainWindow->DialogBox( -title => "Error",
                                        -buttons => ["OK"],
-                                        -font => $opt{'font-var'},
                                      );
 
     my $label = $dbox->add( 'Label',
