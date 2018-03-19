230714459219b44d962b915e7f76c5bdc739033a
Graham Bell <g.bell@eaobservatory.org>
2018-03-19 09:50:35 -1000

    OMP::General: add another UKIRT project pattern

--- lib/OMP/General.pm
+++ lib/OMP/General.pm
@@ -404,7 +404,7 @@ sub extract_projectid {
     qr{ \b
         ( u
           / 1[3-9][ab]
-          / (?: lm | ua | na | osu | nau | nav ) (?: [0-9][1-9] | [1-9]0 ) [a-z]?
+          / (?: lm | ua | na | osu | nau | nav | ncu ) (?: [0-9][1-9] | [1-9]0 ) [a-z]?
         )
         \b
       }xi;
