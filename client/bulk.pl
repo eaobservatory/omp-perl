7c19326c1fb97af7f5b5d20929fc1988f0973d51
Graham Bell <g.bell@eaobservatory.org>
2019-01-25 09:29:49 -1000

    client/bulk: Tidy script
    (cherry picked from commit e1fd417ff6e5b0859675cedb8eaa5020354837e2)

--- client/bulk.pl
+++ client/bulk.pl
@@ -6,7 +6,7 @@ bulk - Submit a feedback comment to multiple projects
 
 =head1 SYNOPSIS
 
-bulk -comment comment.txt -projects projects.txt -userid deloreyk
+    bulk -comment comment.txt -projects projects.txt -userid deloreyk
 
 =head1 DESCRIPTION
 
@@ -61,17 +61,132 @@ In the text file containing the feedback comment, the first line should
 be the subject of the comment.  Any lines following that line will be used
 as the message body of the comment.
 
-  Comment Subject
-  Comment text line 1.
-  Comment text line 2.
+    Comment Subject
+    Comment text line 1.
+    Comment text line 2.
 
 In the project IDs text file, the ID of each project for which the comment
 is being submitted should be provided on its own line.
 
-  U/05A/1
-  U/05A/2
-  M05AC01
-  M05AU02
+    U/05A/1
+    U/05A/2
+    M05AC01
+    M05AU02
+
+=cut
+
+use warnings;
+use strict;
+
+use Getopt::Long;
+use IO::File;
+use Pod::Usage;
+use Term::ReadLine;
+
+# Locate the OMP software through guess work
+use FindBin;
+use lib "$FindBin::RealBin/../lib";
+
+use OMP::Constants qw/:fb/;
+use OMP::FBServer;
+use OMP::Password;
+use OMP::UserServer;
+use OMP::ProjServer;
+
+our $VERSION = '2.000';
+
+# Options
+my ($commentfile, $help, $man, $projectsfile, $author, $version, $type);
+
+GetOptions(
+    'comment=s' => \$commentfile,
+    'help' => \$help,
+    'man' => \$man,
+    'projects=s' => \$projectsfile,
+    'userid=s' => \$author,
+    'version' => \$version,
+    'support' => sub {$type = 'support'},
+) or pod2usage(-exitstatus => 1, -verbose => 0);
+
+pod2usage(-exitstatus => 0, -verbose => 99, -sections => [
+    qw/SYNOPSIS OPTIONS FORMAT/]) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+
+if ($version) {
+    print "bulk - Submit a feedback comment to multiple projects\n";
+    print "Version: $VERSION\n";
+    exit;
+}
+
+# Die if missing certain arguments
+unless (defined $commentfile and defined $projectsfile and defined $author) {
+    die "The following arguments are not optional: -comment, -projects, -userid";
+}
+
+# Setup term
+my $term = new Term::ReadLine('Submit a feedback comment to multiple projects');
+
+# Get the author user
+my $user = OMP::UserServer->getUser($author);
+die "No such user [$author]"
+    unless ($user);
+
+# Verify staff password
+my $attribs = $term->Attribs;
+$attribs->{redisplay_function} = $attribs->{shadow_redisplay};
+my $passwd = $term->readline('Please enter the staff password: ');
+$attribs->{redisplay_function} = $attribs->{rl_redisplay};
+OMP::Password->verify_staff_password($passwd);
+
+# Get the projects
+my $proj_fh = new IO::File($projectsfile, 'r')
+    or die "Couldn't open file [$projectsfile]: $!";
+
+my @projects;
+foreach my $id (<$proj_fh>) {
+    chomp $id;
+
+    next
+        if $id =~ m/^\s*#/
+        or $id =~ m/^\s*$/;
+
+    push @projects, $id;
+}
+
+close($proj_fh);
+
+# Verify that the projects exist
+foreach my $projectid (@projects) {
+    die "Project does not exist [$projectid]"
+        unless OMP::ProjServer->verifyProject($projectid);
+}
+
+# Create the comment
+my $comment_fh = new IO::File($commentfile, 'r')
+    or die "Couldn't open file [$commentfile]: $!";
+my @comment = <$comment_fh>;
+close($comment_fh);
+
+my $subject = shift @comment;
+
+my %comment = (
+    text => join("", @comment),
+    author => $user,
+    program => 'COMMENT_CLIENT',
+    subject => $subject,
+);
+
+$comment{'status'} = OMP__FB_SUPPORT
+    if defined $type && $type eq 'support';
+
+# Submit the comment for each project
+foreach my $projectid (@projects) {
+    OMP::FBServer->addComment($projectid, \%comment);
+}
+
+print "The comment has been submitted.\n";
+
+__END__
 
 =head1 AUTHOR
 
@@ -96,110 +211,3 @@ this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place,Suite 330, Boston, MA  02111-1307, USA
 
 =cut
-
-use warnings;
-use strict;
-
-use Getopt::Long;
-use Pod::Usage;
-use Term::ReadLine;
-
-# Locate the OMP software through guess work
-use FindBin;
-use lib "$FindBin::RealBin/../lib";
-
-use OMP::Constants qw[ :fb ];
-use OMP::FBServer;
-use OMP::Password;
-use OMP::UserServer;
-use OMP::ProjServer;
-
-our $VERSION = '2.000';
-
-# Options
-my ($commentfile, $help, $man, $projectsfile, $author, $version, $type);
-
-GetOptions(
-                        "comment=s" => \$commentfile,
-                        "help" => \$help,
-                        "man" => \$man,
-                        "projects=s" => \$projectsfile,
-                        "userid=s" => \$author,
-                        "version" => \$version,
-                        "support" => sub { $type = 'support' },
-) or pod2usage(-exitstatus => 1, -verbose => 0);
-
-pod2usage(-exitstatus => 0, -verbose => 99, -sections => [
-    qw/SYNOPSIS OPTIONS FORMAT/]) if $help;
-pod2usage(-exitstatus => 0, -verbose => 2) if $man;
-
-if ($version) {
-  print "bulk - Submit a feedback comment to multiple projects\n";
-  print "Version: ", $VERSION, "\n";
-  exit;
-}
-
-# Die if missing certain arguments
-unless (defined $commentfile and defined $projectsfile and defined $author) {
-  die "The following arguments are not optional: -comment, -projects, -userid\n";
-}
-
-# Setup term
-my $term = new Term::ReadLine 'Submit a feedback comment to multiple projects';
-
-# Get the author user
-my $user = OMP::UserServer->getUser($author);
-die ("No such user [$author]\n")
-  unless ($user);
-
-# Verify staff password
-my $attribs = $term->Attribs;
-$attribs->{redisplay_function} = $attribs->{shadow_redisplay};
-my $passwd = $term->readline( "Please enter the staff password: ");
-$attribs->{redisplay_function} = $attribs->{rl_redisplay};
-OMP::Password->verify_staff_password($passwd);
-
-# Get the projects
-open(my $proj_fh, '<', $projectsfile) or die ("Couldn't open file [$projectsfile]: $!\n");
-
-my @projects;
-while ( my $id = <$proj_fh> ) {
-
-  next
-    if $id =~ m/^\s*#/
-    or $id =~ m/^\s*$/;
-
-  push @projects, $id;
-}
-chomp(@projects);
-
-close($proj_fh);
-
-# Verify that the projects exist
-for my $projectid (@projects) {
-  my $verify = OMP::ProjServer->verifyProject($projectid);
-  die ("Project does not exist [$projectid]")
-    unless ($verify);
-}
-
-# Create the comment
-open(my $comment_fh, '<', $commentfile) or die ("Couldn't open file [$commentfile]: $!\n");
-my @comment = <$comment_fh>; # Slurrrrp!
-close($comment_fh);
-
-my $subject = shift(@comment);
-
-my %comment = (text => join("", @comment),
-               author => $user,
-               program => 'COMMENT_CLIENT',
-               subject => $subject,);
-
-defined $type && $type eq 'support' and
-  $comment{'status'} = OMP__FB_SUPPORT;
-
-# Submit the comment for each project
-for my $projectid (@projects) {
-  OMP::FBServer->addComment($projectid, \%comment);
-}
-
-print "The comment has been submitted.\n";
