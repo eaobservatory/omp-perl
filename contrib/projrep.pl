108a3d256b2172ddb0f92ba9425f6b1bfc14515a
Sarah F. Graves <s.graves@eaobservatory.org>
2019-03-06 14:15:52 -1000

    contrib/projrep.pl: update projrep.pl to support multiple shifts
    This will now show the overall summary for the week, and then below
    it will print a briefer summary for each shift (if more than one shift).

--- contrib/projrep.pl
+++ contrib/projrep.pl
@@ -21,9 +21,6 @@ use OMP::NightRep;
 use strict;
 use Getopt::Long;
 
-my $total = 0;
-my $total_pending = 0;
-
 # Options
 my ($help, $man, $tel, $ut, $days);
 my $status = GetOptions("help" => \$help,
@@ -68,48 +65,156 @@ my $nr = OMP::NightRep->new(date => $startut,
 
 my $countrylist = "DDT EC CA INT NL UH UK PI JLS GT JAC LAP";
 
+
+#print scalar $nr->astext();
+
+
 my %acct = $nr->accounting_db('byproject');
 
 my $faultloss = $nr->timelost->hours;
 my $technicalloss = $nr->timelost('technical')->hours;
-my %items;
+#my %items;
 
-$total += $faultloss;
+#my $total = 0;
+#my $total_pending = 0;
 
-my $total_proj = 0.0;
+#$total += $faultloss;
 
-foreach my $proj (keys %acct) {
-  next if $proj =~ /^$telescope/;
-  my $account = $acct{$proj};
-  $total += $account->{total}->hours;
-  $total_proj += $account->{total}->hours;
+#my $total_proj = 0.0;
 
-  # No determine_country method exists, so we'll get project
-  # details instead
-  my $details = OMP::ProjServer->projectDetailsNoAuth($proj, "object");
 
-  my $country = $details->country;
-  #countrylist .= " $country" if ($country !~ /$countrylist/);
 
-  my $pending;
-  if  ($account->{pending}) {
-    $total_pending += $account->{pending}->hours;
-    $pending = $account->{pending}->hours;
-  }
+sub print_reporting_breakdown {
+    my $faultloss = $_[0];
+    my $technicalloss = $_[1];
+    my $longrep = $_[2];
+    my %acct = %{$_[3]};
 
-  my $time = $account->{total}->hours;
-  $time *= -1 if ($pending);
-  $items{"$proj"} = "${time}\@${country}";
-}
+    my $total = 0;
+    my $total_pending = 0;
+    my $total_proj = 0.0;
+    my %items;
+    $total += $faultloss;
 
-my @country_totals;
-foreach my $country ( split(/\s+/,$countrylist) ) {
-   my $ctotal = 0.0;
-   foreach my $proj (keys %acct) {
-      my ($ptime, $pcountry) = split(/\@/,$items{$proj});
-      $ctotal += abs($ptime) if ($pcountry eq $country);
-   }
-   push @country_totals, $ctotal;
+    foreach my $proj (keys %acct) {
+        next if $proj =~ /^$telescope/;
+        my $account = $acct{$proj};
+        $total += $account->{total}->hours;
+        $total_proj += $account->{total}->hours;
+
+        # No determine_country method exists, so we'll get project
+        # details instead
+        my $details = OMP::ProjServer->projectDetailsNoAuth($proj, "object");
+
+        my $country = $details->country;
+        #countrylist .= " $country" if ($country !~ /$countrylist/);
+
+        my $pending;
+        if  ($account->{pending}) {
+            $total_pending += $account->{pending}->hours;
+            $pending = $account->{pending}->hours;
+        }
+
+        my $time = $account->{total}->hours;
+        $time *= -1 if ($pending);
+        $items{"$proj"} = "${time}\@${country}";
+    }
+
+    my @country_totals;
+    foreach my $country ( split(/\s+/,$countrylist) ) {
+        my $ctotal = 0.0;
+        foreach my $proj (keys %acct) {
+            my ($ptime, $pcountry) = split(/\@/,$items{$proj});
+            $ctotal += abs($ptime) if ($pcountry eq $country);
+        }
+        push @country_totals, $ctotal;
+    }
+
+
+    foreach my $proj (qw/WEATHER OTHER EXTENDED CAL _SHUTDOWN/) {
+        my $time = 0.0;
+        my $pending;
+        if (exists $acct{$telescope.$proj}) {
+            $time = $acct{$telescope.$proj}->{total}->hours;
+            if ($acct{$telescope.$proj}->{pending}) {
+                $pending += $acct{$telescope.$proj}->{pending}->hours;
+            }
+            $total += $time unless ($proj eq 'EXTENDED' or $proj eq '_SHUTDOWN');
+        }
+
+        $time *= -1 if ($pending);
+        $items{lc("$proj")} = $time;
+    }
+
+    if ($longrep ) {
+        print  "Total time lost to weather                   W:  ";
+        printf "%6.2f hrs",abs($items{"weather"});
+        print  "  \[pending\]" if ($items{"weather"} < 0);
+        print  "\n";
+    }
+    printf "Time lost to faults                          F:  %6.2f hrs\n",
+        $faultloss;
+    if ($longrep) {
+        printf "  technical faults         F1:  %6.2f hrs\n",
+            $technicalloss;
+        printf "  non-technical faults     F2:  %6.2f hrs\n",
+            $faultloss-$technicalloss;
+    }
+    printf "Prime time observed on Projects              P:  %6.2f hrs\n",
+            $total_proj;
+
+    if ($longrep) {
+        print  "Other Time                                   O:  ";
+        printf "%6.2f hrs",abs($items{"other"});
+        print  "  \[pending\]" if ($items{"other"} < 0);
+        print  "\n";
+
+        print  "Calibrations                                 C:  ";
+        printf "%6.2f hrs",abs($items{"cal"});
+        print  "  \[pending\]" if ($items{"cal"} < 0);
+        print  "\n";
+
+        print  "                                                 ------\n";
+    }
+    printf "Total                                        T:  %6.2f hrs\n",
+        $total;
+    if ($longrep) {
+        print  "Facility Closure                             S:  ";
+        printf "%6.2f hrs",abs($items{"_shutdown"});
+        print  "  \[pending\]" if ($items{"_shutdown"} < 0);
+        print  "\n";
+    }
+    printf "Clear time lost to faults              F/(T-W):  %6.2f \%\n",
+        100.0*$faultloss/($total-abs($items{"weather"}));
+    if ($longrep) {
+        printf "Clear time lost to technical faults   F1/(T-W):  %6.2f \%\n",
+        100.0*$technicalloss/($total-abs($items{"weather"}));
+        print  "\n";
+
+        print  "Extended Time                                E:  ";
+        printf "%6.2f hrs",abs($items{"extended"});
+        print  "  \[pending\]" if ($items{"extended"} < 0);
+        print  "\n\n";
+
+
+        my $cnr = 0;
+        foreach my $country ( split(/\s/,$countrylist) ) {
+            my $done_first = 0;
+            foreach my $proj (keys %acct) {
+                my ($ptime, $pcountry) = split(/\@/,$items{$proj});
+                if ($pcountry eq $country) {
+                    printf "%-10.10s %6.2f hrs", $proj, abs($ptime);
+                    printf "    %-3.3s %6.2f hrs", $country, $country_totals[$cnr]
+                        if ($done_first eq 0);
+                    $done_first++;
+                    print  "   [pending]" if ($ptime < 0);
+                    print  "\n";
+                }
+            }
+            print "\n" if ($done_first > 0);
+            $cnr++;
+        }
+    }
 }
 
 print
@@ -119,84 +224,48 @@ qq{__________________________________________________________________________
 ...Observing Summary:
 
 };
+print_reporting_breakdown($faultloss, $technicalloss, 1, \%acct);
+print "...Observing Summary By shift:\n\n";
 
-foreach my $proj (qw/WEATHER OTHER EXTENDED CAL _SHUTDOWN/) {
-  my $time = 0.0;
-  my $pending;
-  if (exists $acct{$telescope.$proj}) {
-    $time = $acct{$telescope.$proj}->{total}->hours;
-    if ($acct{$telescope.$proj}->{pending}) {
-      $pending += $acct{$telescope.$proj}->{pending}->hours;
-    }
-    $total += $time unless ($proj eq 'EXTENDED' or $proj eq '_SHUTDOWN');
+print "";
+
+my %shiftacct = $nr->accounting_db('byshftprj');
+
+my %faultlosses = $nr->timelostbyshift;
+my %techfaultlosses = $nr->timelostbyshift('technical');
+my @faultshifts = (keys %faultlosses);
+my @shifts = (keys %shiftacct);
+
+
+
+for my $shift (@faultshifts) {
+    if((! exists($shiftacct{$shift}) ) && ($shift ne '')) {
+          push @shifts, $shift;
+      }
   }
 
-  $time *= -1 if ($pending);
-  $items{lc("$proj")} = $time;
+if (@shifts > 1) {
+   for my $shift (@shifts) {
+      print "_________________________________________________________________________\n";
+      print "$shift\n";
+      if ( exists $faultlosses{$shift} ) {
+
+          $faultloss = $faultlosses{$shift}->hours;
+          $technicalloss = $techfaultlosses{$shift}->hours;
+    } else {
+     $faultloss = 0.0;
+     $technicalloss = 0.0;
+    }
+    if (exists $shiftacct{$shift} ) {
+       %acct = %{$shiftacct{$shift}};
+} else {
+       %acct;
+}
+    print_reporting_breakdown($faultloss, $technicalloss, 0, \%acct);
+}
 }
 
-print  "Total time lost to weather                   W:  ";
-printf "%6.2f hrs",abs($items{"weather"});
-print  "  \[pending\]" if ($items{"weather"} < 0);
-print  "\n";
-
-printf "Time lost to faults                          F:  %6.2f hrs\n",
-             $faultloss;
-printf "  technical faults         F1:  %6.2f hrs\n",
-             $technicalloss;
-printf "  non-technical faults     F2:  %6.2f hrs\n",
-             $faultloss-$technicalloss;
-
-printf "Prime time observed on Projects              P:  %6.2f hrs\n",
-             $total_proj;
-
-print  "Other Time                                   O:  ";
-printf "%6.2f hrs",abs($items{"other"});
-print  "  \[pending\]" if ($items{"other"} < 0);
-print  "\n";
-
-print  "Calibrations                                 C:  ";
-printf "%6.2f hrs",abs($items{"cal"});
-print  "  \[pending\]" if ($items{"cal"} < 0);
-print  "\n";
-
-print  "                                                 ------\n";
-printf "Total                                        T:  %6.2f hrs\n",
-             $total;
-
-print  "Facility Closure                             S:  ";
-printf "%6.2f hrs",abs($items{"_shutdown"});
-print  "  \[pending\]" if ($items{"_shutdown"} < 0);
-print  "\n";
-
-printf "Clear time lost to faults              F/(T-W):  %6.2f \%\n",
-       100.0*$faultloss/($total-abs($items{"weather"}));
-printf "Clear time lost to technical faults   F1/(T-W):  %6.2f \%\n",
-       100.0*$technicalloss/($total-abs($items{"weather"}));
-print  "\n";
-
-print  "Extended Time                                E:  ";
-printf "%6.2f hrs",abs($items{"extended"});
-print  "  \[pending\]" if ($items{"extended"} < 0);
-print  "\n\n";
-
-my $cnr = 0;
-foreach my $country ( split(/\s/,$countrylist) ) {
-   my $done_first = 0;
-   foreach my $proj (keys %acct) {
-      my ($ptime, $pcountry) = split(/\@/,$items{$proj});
-      if ($pcountry eq $country) {
-        printf "%-10.10s %6.2f hrs", $proj, abs($ptime);
-        printf "    %-3.3s %6.2f hrs", $country, $country_totals[$cnr]
-            if ($done_first eq 0);
-        $done_first++;
-        print  "   [pending]" if ($ptime < 0);
-        print  "\n";
-      }
-   }
-   print "\n" if ($done_first > 0);
-   $cnr++;
-}
+print "_________________________________________________________________________\n";
 
 
 sub help () {
