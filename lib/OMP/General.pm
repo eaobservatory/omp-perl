52cdab08df9b9853f156bc48ea9caa503e949a65
Graham Bell <g.bell@eaobservatory.org>
2019-07-16 08:58:35 -1000

    OMP::General: correct POD error
    There was an =item outside of the =over block.

--- lib/OMP/General.pm
+++ lib/OMP/General.pm
@@ -1279,8 +1279,6 @@ Though the basic idea can be generalized, better solution (even in
 this case) would be to use L<Math::Units>, L<Physical::Units>, or
 similar.
 
-=back
-
 =cut
 
 BEGIN {
@@ -1315,7 +1313,6 @@ BEGIN {
   }
 }
 
-
 =item B<rot13>
 
 Given a list of strings, returns list of ROT-13 obfuscated strings.
@@ -1337,6 +1334,7 @@ sub rot13 {
   return @in;
 }
 
+=back
 
 =head1 AUTHORS
 
