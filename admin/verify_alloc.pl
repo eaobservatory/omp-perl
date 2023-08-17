8aac8b0fac497008c34ee37c6db7b281402a556e
Graham Bell <g.bell@eaobservatory.org>
2023-08-17 13:00:48 -1000

    admin: Read project definition files as UTF-8

--- admin/verify_alloc.pl
+++ admin/verify_alloc.pl
@@ -40,12 +40,17 @@ BEGIN {
 use Config::IniFiles;
 use OMP::UserServer;
 use Data::Dumper;
+use IO::File;
 
 # hard-wire for testing
 my $file = "data/alloc04a_ukirt.ini";
 
 my %alloc;
-tie %alloc, 'Config::IniFiles', ( -file => $file );
+my $fh = IO::File->new($file, 'r');
+die "Could not open file '$file'" unless defined $fh;
+$fh->binmode(':utf8');
+tie %alloc, 'Config::IniFiles', ( -file => $fh );
+$fh->close();
 
 # Loop over all projects
 my %users;
